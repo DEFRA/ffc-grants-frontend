519fa2f0df56f0b5cbcadfe23920d9b6c1bfcc32
Madiha <45498508+diya2214@users.noreply.github.com>
2021-03-02 13:26:21 +0000

    unit tests for irrigated farming and project pages (#10)

---
+++ test/integration/narrow/routes/project.test.js
@@ -0,0 +1,57 @@
+describe('Project details page', () => {
+    let server
+    const createServer = require('../../../../app/server')
+
+    beforeEach(async () => {
+        server = await createServer()
+        await server.start()
+    })
+
+    it('should load page successfully', async () => {
+        const options = {
+            method: 'GET',
+            url: '/project'
+        }
+
+        const response = await server.inject(options)
+        expect(response.statusCode).toBe(200)
+    })
+
+    it('should returns error message if no option is selected', async () => {
+        const postOptions = {
+            method: 'POST',
+            url: '/project',
+            payload: {}
+        }
+
+        const postResponse = await server.inject(postOptions)
+        expect(postResponse.statusCode).toBe(200)
+        expect(postResponse.payload).toContain('Please select an option')
+    })
+
+    it('should store user response and redirects to irrigated crops page', async () => {
+        const postOptions = {
+            method: 'POST',
+            url: '/project',
+            payload: { project: 'Improve irrigation efficiency' }
+        }
+
+        const postResponse = await server.inject(postOptions)
+        expect(postResponse.statusCode).toBe(302)
+        expect(postResponse.headers.location).toBe('./irrigated-crops')
+    })
+
+    it('should display the error summary if more than two options are selected', async () => {
+        const postOptions = {
+            method: 'POST',
+            url: '/project',
+            payload: { project: ['some option-1', 'some option-2', 'some option-3'] }
+        }
+
+        const postResponse = await server.inject(postOptions)
+        expect(postResponse.payload).toContain('There is a problem')
+    })
+    afterEach(async () => {
+        await server.stop()
+    })
+})
