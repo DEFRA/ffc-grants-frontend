0a401236e9b9436f31dc6a1229e17da3390bf9c2
Paul Andrews <paul.andrews@defra.gov.uk>
2021-03-04 14:46:24 +0000

    Use status code of response as the check

--- app/routes/message-test1.js
+++ app/routes/message-test1.js
@@ -19,13 +19,14 @@ module.exports = [
       const msg = { body: { key } }
       await testSender(msg)
 
-      let value
+      let value, statusCode
 
       do {
         const url = `http://host.docker.internal:3001/test1?key=${key}`
-        const { payload } = await Wreck.get(url, { json: true })
-        value = payload.value
-      } while (!value)
+        const response = await Wreck.get(url, { json: true })
+        value = response.payload.value
+        statusCode = response.res.statusCode
+      } while (statusCode === 202)
 
       request.yar.set('serverMsg1', value)
 
