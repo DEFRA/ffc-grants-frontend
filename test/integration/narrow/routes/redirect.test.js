9e27e9490829289ac4a5d48257e64fd6cf891d5e
diya2214 <madihakhn@yahoo.com>
2021-03-04 12:07:17 +0000

    redirect test page

---
+++ test/integration/narrow/routes/redirect.test.js
@@ -0,0 +1,25 @@
+describe('redirect Page', () => {
+    let server
+    const createServer = require('../../../../app/server')
+
+    beforeEach(async () => {
+        server = await createServer()
+        await server.start()
+    })
+
+    it('should redirect to /start ', async () => {
+        const options = {
+            method: 'GET',
+            url: '/'
+        }
+
+        const response = await server.inject(options)
+        expect(response.statusCode).toBe(302)
+        expect(response.headers.location).toBe('./start')
+
+    })
+
+    afterEach(async () => {
+        await server.stop()
+    })
+})
