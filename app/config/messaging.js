42bd6f1e06055b6db0868cc888adb590b8f94515
Paul Andrews <paul.andrews@defra.gov.uk>
2021-03-09 06:44:58 +0000

    Queue config

--- app/config/messaging.js
+++ app/config/messaging.js
@@ -9,12 +9,23 @@ const sharedConfig = {
 const msgTypePrefix = 'uk.gov.ffc.grants'
 
 module.exports = {
-  fixmeQueue: {
-    address: process.env.FIXME_QUEUE_ADDRESS,
-    type: FIXME,
+  eligibilityAnswersQueue: {
+    address: process.env.ELIGIBILITY_ANSWERS_QUEUE_ADDRESS,
+    type: 'queue',
     ...sharedConfig
   },
-  fixmeMsgType: `${msgTypePrefix}.fixme.fixme`,
+  projectDetailsQueue: {
+    address: process.env.PROJECT_DETAILS_QUEUE_ADDRESS,
+    type: 'queue',
+    ...sharedConfig
+  },
+  contactDetailsQueue: {
+    address: process.env.CONTACT_DETAILS_QUEUE_ADDRESS,
+    type: 'queue',
+    ...sharedConfig
+  },
+  eligibilityAnswersMsgType: `${msgTypePrefix}.eligibility.details`,
+  projectDetailsMsgType: `${msgTypePrefix}.project.details`,
+  contactDetailsMsgType: `${msgTypePrefix}.contact.details`,
   msgSrc: 'ffc-grants-frontend'
 }
-
