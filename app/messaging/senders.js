6bfb5094be0816c4d61f234159b40cf67bf15364
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2021-03-09 07:23:34 +0000

    Add Service Bus code and config (#15)
    https://eaflood.atlassian.net/browse/FFTF-304
    
    * Add ffc-messaging and application insights packages
    * Update to node 14 version of Docker image
    * Update NPM packages
    * Add PR Queue provisioning
    * Add queue env vars
    * Add Azure Identity to Helm chart
    * Add message senders

---
+++ app/messaging/senders.js
@@ -0,0 +1,46 @@
+const { MessageSender } = require('ffc-messaging')
+const msgCfg = require('../config/messaging')
+
+const eligibilityAnswersSender = new MessageSender(msgCfg.eligibilityAnswersQueue)
+const projectDetailsSender = new MessageSender(msgCfg.projectDetailsQueue)
+const contactDetailsSender = new MessageSender(msgCfg.contactDetailsQueue)
+
+async function stop () {
+  await eligibilityAnswersSender.closeConnection()
+  await projectDetailsSender.closeConnection()
+  await contactDetailsSender.closeConnection()
+}
+
+process.on('SIGTERM', async () => {
+  await stop()
+  process.exit(0)
+})
+
+process.on('SIGINT', async () => {
+  await stop()
+  process.exit(0)
+})
+
+async function sendMsg (sender, msgData, msgType) {
+  const msg = {
+    body: msgData,
+    type: msgType,
+    source: msgCfg.msgSrc
+  }
+
+  console.log('sending message', msg)
+
+  await sender.sendMessage(msg)
+}
+
+module.exports = {
+  sendEligibilityAnswers: async function (eligibilityAnswersData) {
+    await sendMsg(eligibilityAnswersSender, eligibilityAnswersData, msgCfg.eligibilityAnswersMsgType)
+  },
+  sendProjectDetails: async function (projectDetailsData) {
+    await sendMsg(projectDetailsSender, projectDetailsData, msgCfg.projectDetailsMsgType)
+  },
+  sendContactDetails: async function (contactDetailsData) {
+    await sendMsg(contactDetailsSender, contactDetailsData, msgCfg.contactDetailsMsgType)
+  }
+}
