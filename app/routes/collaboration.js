38e834d1e3c164e228b1ad6a8ae5442ef331e771
Madiha <45498508+diya2214@users.noreply.github.com>
2021-03-10 08:49:43 +0000

    FFTF-187: Collaboration Page UI (#20)
    * version update
    
    * helper function
    
    * productivity UI
    
    * unit tests for productivity
    
    * fixing test
    
    * refactoring

---
+++ app/routes/collaboration.js
@@ -0,0 +1,56 @@
+const Joi = require('joi')
+let { setLabelData } = require('../helpers/helper-functions')
+
+function createModel(errorMessage, data) {
+    return {
+        backLink: '/productivity',
+        radios: {
+            classes: '',
+            idPrefix: 'collaboration',
+            name: 'collaboration',
+            fieldset: {
+                legend: {
+                    text: 'Will water be supplied to other farms?',
+                    isPageHeading: true,
+                    classes: 'govuk-fieldset__legend--l'
+                }
+            },
+            hint: {
+                text: "If you intend to supply water via a water sharing agreement as a result of this project."
+            },
+            items: [
+                setLabelData(data, 'Yes'),
+                setLabelData(data,'No') ,
+            ],
+            ...(errorMessage ? { errorMessage: { text: errorMessage } } : {})
+        }
+    }
+}
+
+module.exports = [
+    {
+        method: 'GET',
+        path: '/collaboration',
+        handler: (request, h) => {
+            const collaboration = request.yar.get('collaboration');
+            const data = !!collaboration ? collaboration : null
+            return h.view('collaboration', createModel(null, data))
+        }
+    },
+    {
+        method: 'POST',
+        path: '/collaboration',
+        options: {
+            validate: {
+                payload: Joi.object({
+                    collaboration: Joi.string().required()
+                }),
+                failAction: (request, h) => h.view('collaboration', createModel('Please select an option')).takeover()
+            },
+            handler: (request, h) => {
+                request.yar.set('collaboration', request.payload.collaboration)
+                return h.redirect('./answers')
+            }
+        }
+    }
+]
