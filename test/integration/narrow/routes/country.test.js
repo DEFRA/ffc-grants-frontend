85247e490febd5b2e06c109493cfe6510057b6d4
diya2214 <madihakhn@yahoo.com>
2021-03-15 09:06:53 +0000

    unit tests for country page

--- test/integration/narrow/routes/country.test.js
+++ test/integration/narrow/routes/country.test.js
@@ -1,4 +1,4 @@
-describe('Home test', () => {
+describe('Country Page', () => {
   let server
   const createServer = require('../../../../app/server')
 
@@ -7,7 +7,7 @@ describe('Home test', () => {
     await server.start()
   })
 
-  test('GET / route returns 200', async () => {
+  it('should load country page sucessfully', async () => {
     const options = {
       method: 'GET',
       url: '/country'
@@ -17,6 +17,41 @@ describe('Home test', () => {
     expect(response.statusCode).toBe(200)
   })
 
+  it('should returns error message if no option is selected', async () => {
+    const postOptions = {
+        method: 'POST',
+        url: '/country',
+        payload: { inEngland: null }
+    }
+
+    const postResponse = await server.inject(postOptions)
+    expect(postResponse.statusCode).toBe(200)
+    expect(postResponse.payload).toContain('You must select an option')
+  })
+  
+  it('should returns error message if postcode is not entered for selected yes option ', async () => {
+    const postOptions = {
+        method: 'POST',
+        url: '/country',
+        payload: { inEngland: 'Yes' , project_postcode: ''}
+    }
+
+    const postResponse = await server.inject(postOptions)
+    expect(postResponse.statusCode).toBe(200)
+    expect(postResponse.payload).toContain('If yes, please type in postcode')
+})
+
+it('should store user response and redirects to project details page', async () => {
+  const postOptions = {
+      method: 'POST',
+      url: '/country',
+      payload: { inEngland: 'Yes' , project_postcode: 'XX1 5XX' }
+  }
+
+  const postResponse = await server.inject(postOptions)
+  expect(postResponse.statusCode).toBe(302)
+  expect(postResponse.headers.location).toBe('./project-details')
+})
   afterEach(async () => {
     await server.stop()
   })
