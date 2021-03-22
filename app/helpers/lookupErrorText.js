f5eb41c0f9544055a3853dcf65dd18250b24dcea
Madiha <45498508+diya2214@users.noreply.github.com>
2021-03-22 10:55:02 +0000

    FFTF-195: error handling on irrigated crops page  (#36)
    * error text update
    
    * irrigated crops error update

--- app/helpers/lookupErrorText.js
+++ app/helpers/lookupErrorText.js
@@ -1,7 +1,8 @@
 const errors = {
   'error.inEngland.any.required': 'Select yes if the project is in England',
   'error.projectPostcode.string.pattern.base': 'Enter a postcode, like AA1 1AA',
-  'error.projectPostcode.string.empty': 'Enter a postcode, like AA1 1AA'
+  'error.projectPostcode.string.empty': 'Enter a postcode, like AA1 1AA',
+  'error.irrigatedCrops.any.required':'Select the main crop you will be irrigating'
 
 }
 const lookupErrorText = (key) => {
