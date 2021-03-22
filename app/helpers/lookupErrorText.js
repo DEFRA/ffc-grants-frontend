2d42f4cbe2a91c1d61736e2386948021a2250bfb
diya2214 <madihakhn@yahoo.com>
2021-03-22 11:13:36 +0000

    error text update

--- app/helpers/lookupErrorText.js
+++ app/helpers/lookupErrorText.js
@@ -1,8 +1,8 @@
 const errors = {
   'error.inEngland.any.required': 'Select yes if the project is in England',
   'error.projectPostcode.string.pattern.base': 'Enter a postcode, like AA1 1AA',
-  'error.projectPostcode.string.empty': 'Enter a postcode, like AA1 1AA'
-
+  'error.projectPostcode.string.empty': 'Enter a postcode, like AA1 1AA',
+  'error.farmingType.any.required': 'Select the crops you are growing'
 }
 const lookupErrorText = (key) => {
   return errors[key] || key
