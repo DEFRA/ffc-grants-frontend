fe5cb6f4d738360e63fe00b6d70e059803e30960
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2021-03-30 17:14:44 +0100

    Tidy up

--- app/routes/answers.js
+++ app/routes/answers.js
@@ -1,12 +1,7 @@
-const senders = require('../messaging/senders')
-const createMsg = require('../messaging/create-msg')
-
 module.exports = {
   method: 'GET',
   path: '/answers',
   handler: async (request, h) => {
-    await senders.sendProjectDetails(createMsg.getDesirabilityAnswers(request))
-
     return h.view('answers', {
       output: {
         titleText: 'MEDIUM FIT'
