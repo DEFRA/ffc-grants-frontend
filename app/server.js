fd838503a0d7165ec9c62bbadc0e8cff1d2be635
govTechSatish <80264942+govTechSatish@users.noreply.github.com>
2021-04-06 09:17:11 +0100

    Added NO INDEX
    added No index meta tag and cross origin cookie policy

--- app/server.js
+++ app/server.js
@@ -24,7 +24,9 @@ async function createServer () {
         { key: 'X-Content-Type-Options', value: 'nosniff' },
         // { key: 'Content-Security-Policy', value: 'default-src \'self\'' },
         // { key: 'Feature-Policy', value: 'none' },
-        { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' }
+        { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
+        { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
+        { key: 'X-Robots-Tag', value: 'noindex, nofollow' }
       ]
     }
   })
