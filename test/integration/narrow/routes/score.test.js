7447097c188e972c5962ccb4c5823f4ba8563a3e
ogbk <ogbu.k.olu@gmail.com>
2021-04-06 12:51:17 +0100

    mock page for /score: so I can build contact pages

---
+++ test/integration/narrow/routes/score.test.js
@@ -0,0 +1,37 @@
+describe('Score page', () => {
+  let server
+  const createServer = require('../../../../app/server')
+
+  beforeEach(async () => {
+    server = await createServer()
+    await server.start()
+  })
+
+  it('should load page successfully', async () => {
+    const options = {
+      method: 'GET',
+      url: '/score'
+    }
+
+    const response = await server.inject(options)
+    expect(response.statusCode).toBe(200)
+  })
+
+  /*
+  it('redirects to project business details page', async () => {
+    const postOptions = {
+      method: 'POST',
+      url: '/score',
+      payload: {}
+    }
+
+    const postResponse = await server.inject(postOptions)
+    expect(postResponse.statusCode).toBe(302)
+    expect(postResponse.headers.location).toBe('./business-details')
+  })
+  */
+
+  afterEach(async () => {
+    await server.stop()
+  })
+})
