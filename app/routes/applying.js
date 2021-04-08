d5ee93804f76ef7784b78f82dd023786b9268c66
ogbk <ogbu.k.olu@gmail.com>
2021-04-08 16:00:13 +0100

    code up /applying with test

---
+++ app/routes/applying.js
@@ -0,0 +1,53 @@
+const Joi = require('joi')
+const { setLabelData } = require('../helpers/helper-functions')
+
+function createModel (errorMessage, data) {
+  return {
+    backLink: '/business-details',
+    radios: {
+      classes: '',
+      idPrefix: 'applying',
+      name: 'applying',
+      fieldset: {
+        legend: {
+          text: 'Who is applying for this grant?',
+          isPageHeading: true,
+          classes: 'govuk-fieldset__legend--l'
+        }
+      },
+      items: setLabelData(data, ['Farmer', 'Agent']),
+      ...(errorMessage ? { errorMessage: { text: errorMessage } } : {})
+    }
+  }
+}
+
+module.exports = [
+  {
+    method: 'GET',
+    path: '/applying',
+    handler: (request, h) => {
+      const applying = request.yar.get('applying')
+      const data = applying || null
+      return h.view('applying', createModel(null, data))
+    }
+  },
+  {
+    method: 'POST',
+    path: '/applying',
+    options: {
+      validate: {
+        payload: Joi.object({
+          applying: Joi.string().required()
+        }),
+        failAction: (request, h) =>
+          h
+            .view('applying', createModel('Select who is applying for this grant', null))
+            .takeover()
+      },
+      handler: (request, h) => {
+        request.yar.set('applying', request.payload.applying)
+        return h.redirect('./confirm')
+      }
+    }
+  }
+]
