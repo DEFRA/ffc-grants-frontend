f56c7bdc6154ee6a20491774fb64bdd177a4b784
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2021-04-09 22:26:40 +0100

    Add polling of backend for score data

---
+++ app/config/polling.js
@@ -0,0 +1,5 @@
+module.exports = {
+  interval: process.env.POLLING_INTERVAL,
+  retries: process.env.POLLING_RETRIES,
+  host: process.env.BACKEND_POLLING_HOST
+}
--- app/routes/answers.js
+++ app/routes/answers.js
@@ -1,14 +1,51 @@
 const senders = require('../messaging/senders')
 const createMsg = require('../messaging/create-msg')
+const Wreck = require('@hapi/wreck')
 const questionBank = require('../config/question-bank')
 const { formatApplicationCode } = require('../helpers/helper-functions')
+const pollingConfig = require('../config/polling')
+
+async function getResult (correlationId) {
+  const url = `${pollingConfig.host}/desirability-score?correlationId=${correlationId}`
+
+  for (let i = 0; i < pollingConfig.retries; i++) {
+    await new Promise(resolve => setTimeout(resolve, pollingConfig.interval))
+    try {
+      const response = await Wreck.get(url, { json: true })
+
+      switch (response.res.statusCode) {
+        case 202:
+          console.log('202 received, backend didn\'t have result, continue polling')
+          break
+        case 200:
+          console.log('200 received, got result from backend, stop polling')
+          return response.payload
+        default:
+          console.log('Unhandled status code, stop polling')
+          return null
+      }
+    } catch (err) {
+      // 4xx and 5xx errors will be caught here along with failure to connect
+      console.log(`${err}`)
+      return null
+    }
+  }
+
+  console.log(`Tried getting score ${pollingConfig.retries} times, giving up`)
+  return null
+}
 
 module.exports = {
   method: 'GET',
   path: '/answers',
   handler: async (request, h) => {
     // Always re-calculate our score before rendering this page
-    // TODO const msgData = await senders.sendProjectDetails(createMsg.getDesirabilityAnswers(request), request.yar.id)
+    await senders.sendProjectDetails(createMsg.getDesirabilityAnswers(request), request.yar.id)
+
+    // Poll for backend for results from scoring algorithm
+    // If msgData is null then 500 page will be triggered when trying to access object below
+    const msgData = await getResult(request.yar.id)
+
     const htmlRef = `Your reference number<br><strong>${formatApplicationCode(request.yar.id)}</strong>`
     const questions = msgData.desirability.questions.map(q => {
       const questionBankQ = questionBank.questions.filter(x => x.key === q.key)[0]
@@ -31,177 +68,177 @@ module.exports = {
   }
 }
 
-const msgData = {
-  grantScheme: {
-    key: 'WM01',
-    name: 'Water Management'
-  },
-  desirability: {
-    questions: [
-      {
-        key: 'Q14',
-        answers: [
-          {
-            key: 'Q14',
-            title: 'Project details',
-            input: [
-              {
-                key: 'Q14-A3',
-                value: 'Increase irrigation'
-              }
-            ]
-          }
-        ],
-        rating: {
-          score: 1,
-          band: 'Low'
-        }
-      },
-      {
-        key: 'Q16',
-        answers: [
-          {
-            key: 'Q16a',
-            title: 'How much land is currently irrigated per year?',
-            input: [
-              {
-                key: null,
-                value: 0
-              }
-            ]
-          },
-          {
-            key: 'Q16b',
-            title: 'How much land will be irrigated in total per year under the grant?',
-            input: [
-              {
-                key: null,
-                value: 50
-              }
-            ]
-          }
-        ],
-        rating: {
-          score: 6,
-          band: 'Medium'
-        }
-      },
-      {
-        key: 'Q17',
-        answers: [
-          {
-            key: 'Q17a',
-            title: 'Where does your current irrigation water come from?',
-            input: [
-              {
-                key: 'Q17a-A6',
-                value: 'Not currently irrigating'
-              }
-            ]
-          },
-          {
-            key: 'Q17b',
-            title: 'Where will the irrigation water come from?',
-            input: [
-              {
-                key: 'Q17b-A2',
-                value: 'Bore hole / aquifer'
-              }
-            ]
-          }
-        ],
-        rating: {
-          score: 40,
-          band: 'High'
-        }
-      },
-      {
-        key: 'Q18',
-        answers: [
-          {
-            key: 'Q18a',
-            title: 'What systems are currently used to irrigate?',
-            input: [
-              {
-                key: 'Q18a-A6',
-                value: 'Not currently irrigating'
-              }
-            ]
-          },
-          {
-            key: 'Q18b',
-            title: 'What systems will be used to irrigate?',
-            input: [
-              {
-                key: 'Q18b-A3',
-                value: 'Ebb and flood or capillary bed'
-              }
-            ]
-          }
-        ],
-        rating: {
-          score: 25,
-          band: 'High'
-        }
-      },
-      {
-        key: 'Q19',
-        answers: [
-          {
-            key: 'Q19',
-            title: 'How will the project improve productivity?',
-            input: [
-              {
-                key: 'Q19-A5',
-                value: 'Maintain productivity'
-              },
-              {
-                key: 'Q19-A3',
-                value: 'Increased yield per hectare'
-              }
-            ]
-          }
-        ],
-        rating: {
-          score: 0.5,
-          band: 'Low'
-        }
-      },
-      {
-        key: 'Q20',
-        answers: [
-          {
-            key: 'Q20',
-            title: 'Will water be supplied to other farms?',
-            input: [
-              {
-                key: 'Q20-A1',
-                value: 'Yes'
-              }
-            ]
-          }
-        ],
-        rating: {
-          score: 4,
-          band: 'High'
-        }
-      }
-    ],
-    overallRating: {
-      score: 76.5,
-      band: 'High'
-    }
-  },
-  questionMapping: {
-    Q14: 'project',
-    Q15: 'irrigatedCrops',
-    Q16a: 'irrigatedLandCurrent',
-    Q16b: 'irrigatedLandTarget',
-    Q17a: 'waterSourceCurrent',
-    Q17b: 'waterSourcePlanned',
-    Q18a: 'irrigationCurrent',
-    Q18b: 'irrigationPlanned',
-    Q19: 'productivity',
-    Q20: 'collaboration'
-  }
-}
+// const msgData = {
+//   grantScheme: {
+//     key: 'WM01',
+//     name: 'Water Management'
+//   },
+//   desirability: {
+//     questions: [
+//       {
+//         key: 'Q14',
+//         answers: [
+//           {
+//             key: 'Q14',
+//             title: 'Project details',
+//             input: [
+//               {
+//                 key: 'Q14-A3',
+//                 value: 'Increase irrigation'
+//               }
+//             ]
+//           }
+//         ],
+//         rating: {
+//           score: 1,
+//           band: 'Low'
+//         }
+//       },
+//       {
+//         key: 'Q16',
+//         answers: [
+//           {
+//             key: 'Q16a',
+//             title: 'How much land is currently irrigated per year?',
+//             input: [
+//               {
+//                 key: null,
+//                 value: 0
+//               }
+//             ]
+//           },
+//           {
+//             key: 'Q16b',
+//             title: 'How much land will be irrigated in total per year under the grant?',
+//             input: [
+//               {
+//                 key: null,
+//                 value: 50
+//               }
+//             ]
+//           }
+//         ],
+//         rating: {
+//           score: 6,
+//           band: 'Medium'
+//         }
+//       },
+//       {
+//         key: 'Q17',
+//         answers: [
+//           {
+//             key: 'Q17a',
+//             title: 'Where does your current irrigation water come from?',
+//             input: [
+//               {
+//                 key: 'Q17a-A6',
+//                 value: 'Not currently irrigating'
+//               }
+//             ]
+//           },
+//           {
+//             key: 'Q17b',
+//             title: 'Where will the irrigation water come from?',
+//             input: [
+//               {
+//                 key: 'Q17b-A2',
+//                 value: 'Bore hole / aquifer'
+//               }
+//             ]
+//           }
+//         ],
+//         rating: {
+//           score: 40,
+//           band: 'High'
+//         }
+//       },
+//       {
+//         key: 'Q18',
+//         answers: [
+//           {
+//             key: 'Q18a',
+//             title: 'What systems are currently used to irrigate?',
+//             input: [
+//               {
+//                 key: 'Q18a-A6',
+//                 value: 'Not currently irrigating'
+//               }
+//             ]
+//           },
+//           {
+//             key: 'Q18b',
+//             title: 'What systems will be used to irrigate?',
+//             input: [
+//               {
+//                 key: 'Q18b-A3',
+//                 value: 'Ebb and flood or capillary bed'
+//               }
+//             ]
+//           }
+//         ],
+//         rating: {
+//           score: 25,
+//           band: 'High'
+//         }
+//       },
+//       {
+//         key: 'Q19',
+//         answers: [
+//           {
+//             key: 'Q19',
+//             title: 'How will the project improve productivity?',
+//             input: [
+//               {
+//                 key: 'Q19-A5',
+//                 value: 'Maintain productivity'
+//               },
+//               {
+//                 key: 'Q19-A3',
+//                 value: 'Increased yield per hectare'
+//               }
+//             ]
+//           }
+//         ],
+//         rating: {
+//           score: 0.5,
+//           band: 'Low'
+//         }
+//       },
+//       {
+//         key: 'Q20',
+//         answers: [
+//           {
+//             key: 'Q20',
+//             title: 'Will water be supplied to other farms?',
+//             input: [
+//               {
+//                 key: 'Q20-A1',
+//                 value: 'Yes'
+//               }
+//             ]
+//           }
+//         ],
+//         rating: {
+//           score: 4,
+//           band: 'High'
+//         }
+//       }
+//     ],
+//     overallRating: {
+//       score: 76.5,
+//       band: 'High'
+//     }
+//   },
+//   questionMapping: {
+//     Q14: 'project',
+//     Q15: 'irrigatedCrops',
+//     Q16a: 'irrigatedLandCurrent',
+//     Q16b: 'irrigatedLandTarget',
+//     Q17a: 'waterSourceCurrent',
+//     Q17b: 'waterSourcePlanned',
+//     Q18a: 'irrigationCurrent',
+//     Q18b: 'irrigationPlanned',
+//     Q19: 'productivity',
+//     Q20: 'collaboration'
+//   }
+// }
