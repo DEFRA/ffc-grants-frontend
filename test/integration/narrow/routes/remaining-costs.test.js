e2824ab46142970dd025f3d650963a3b9823a720
ogbk <ogbu.k.olu@gmail.com>
2021-04-09 12:32:25 +0100

    fix /remaining-costs.test.js

--- test/integration/narrow/routes/remaining-costs.test.js
+++ test/integration/narrow/routes/remaining-costs.test.js
@@ -9,37 +9,11 @@ describe('Remaining costs page', () => {
   })
 
   it('should load page successfully', async () => {
-    const options = {
-      method: 'GET',
-      url: '/remaining-costs'
-    }
-    const response = await server.inject(options)
-    expect(response.statusCode).toBe(302)
-  })
-
-  it('should return an error message if no option is selected', async () => {
     const postOptions = {
       method: 'POST',
       url: '/remaining-costs',
       payload: { crumb: crumToken },
-      headers: {
-        cookie: 'crumb=' + crumToken
-      }
-    }
-
-    const postResponse = await server.inject(postOptions)
-    expect(postResponse.statusCode).toBe(200)
-    expect(postResponse.payload).toContain('Select yes if you can pay the remaining costs without using any other grant money')
-  })
-
-  it('should show elimination message if the user select NO', async () => {
-    const postOptions = {
-      method: 'POST',
-      url: '/remaining-costs',
-      payload: { remainingCosts: 'No', crumb: crumToken },
-      headers: {
-        cookie: 'crumb=' + crumToken
-      }
+      headers: { cookie: 'crumb=' + crumToken }
     }
 
     const postResponse = await server.inject(postOptions)
@@ -47,19 +21,56 @@ describe('Remaining costs page', () => {
     expect(postResponse.payload).toContain('Can you pay the remaining costs of')
   })
 
-  it('should store valid user input and redirect to project grant page', async () => {
+  it('redirects to /project-cost if projectCost value has not been saved', async () => {
+    const options = {
+      method: 'GET',
+      url: '/remaining-costs',
+      payload: { crumb: crumToken },
+      headers: { cookie: 'crumb=' + crumToken }
+    }
+
+    const response = await server.inject(options)
+    expect(response.statusCode).toBe(302)
+    expect(response.headers.location).toBe('./project-cost')
+  })
+
+  it('should return an error message if no option is selected', async () => {
     const postOptions = {
       method: 'POST',
       url: '/remaining-costs',
-      payload: { remainingCosts: 'Yes', crumb: crumToken },
-      headers: {
-        cookie: 'crumb=' + crumToken
-      }
+      payload: { crumb: crumToken },
+      headers: { cookie: 'crumb=' + crumToken }
     }
 
     const postResponse = await server.inject(postOptions)
     expect(postResponse.statusCode).toBe(200)
-    // expect(postResponse.headers.location).toBe('./planning-permission')
+    expect(postResponse.payload).toContain('Select yes if you can pay the remaining costs without using any other grant money')
+  })
+
+  it('user selects NO -> show elimination message', async () => {
+    const postOptions = {
+      method: 'POST',
+      url: '/remaining-costs',
+      payload: { payRemainingCosts: 'No', crumb: crumToken },
+      headers: { cookie: 'crumb=' + crumToken }
+    }
+
+    const postResponse = await server.inject(postOptions)
+    expect(postResponse.statusCode).toBe(200)
+    expect(postResponse.payload).toContain('You cannot apply for a grant from this scheme')
+  })
+
+  it('user selects YES -> store valid user input and redirect to planning-permission', async () => {
+    const postOptions = {
+      method: 'POST',
+      url: '/remaining-costs',
+      payload: { payRemainingCosts: 'Yes', crumb: crumToken },
+      headers: { cookie: 'crumb=' + crumToken }
+    }
+
+    const postResponse = await server.inject(postOptions)
+    expect(postResponse.statusCode).toBe(302)
+    expect(postResponse.headers.location).toBe('./planning-permission')
   })
 
   afterEach(async () => {
