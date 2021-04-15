111afddc4178c91af417eba9cb018d60ea28cfc5
Madiha <45498508+diya2214@users.noreply.github.com>
2021-04-15 17:08:40 +0100

    FFTF-441: Fix postcode label (#81)
    * fix postcode label

--- app/helpers/helper-functions.js
+++ app/helpers/helper-functions.js
@@ -32,13 +32,13 @@ function getPostCodeHtml (postcodeData, error) {
   return !error
     ? `<div>
         <label class="govuk-label" for="projectPostcode">
-          Enter Postcode
+          In which postcode will the project take place?<br/><br/> Postcode
         </label>
         <input class="govuk-input govuk-!-width-one-third" id="projectPostcode" name="projectPostcode" value="${postcode}">
       </div>`
     : `<div class="govuk-form-group--error">
         <label class="govuk-label" for="projectPostcode">
-          Enter Postcode
+          In which postcode will the project take place?<br/><br/> Postcode
         </label>
         <span id="post-code-error" class="govuk-error-message">
           <span class="govuk-visually-hidden">
