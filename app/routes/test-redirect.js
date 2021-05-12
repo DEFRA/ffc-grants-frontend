cee18e2f477653bffc6a8a0456d5ca5eb0c58831
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2021-05-12 17:13:17 +0100

    Remove routes added for testing WAF (#123)
    * Remove testing routes and restore 2 replicas

--- app/routes/test-redirect.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-  {
-    method: 'GET',
-    path: '/test-redirect',
-    handler: (request, h) => {
-      return h.response('You were redirected here from test-post2').code(200)
-    }
-  }
-]
