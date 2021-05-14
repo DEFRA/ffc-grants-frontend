b7cb968f95cefd6e1e838b1952092537abca503a
diya2214 <madihakhn@yahoo.com>
2021-05-14 15:40:14 +0100

    update test

--- test/unit/configs/auth.test.js
+++ test/unit/configs/auth.test.js
@@ -1,6 +1,16 @@
 const config = require('../../../app/config/auth')
 
 describe('get auth config', () => {
+  const OLD_ENV = process.env
+
+  beforeEach(() => {
+    jest.resetModules()
+    process.env = { ...OLD_ENV }
+  })
+
+  afterAll(() => {
+    process.env = OLD_ENV
+  })
   test('Should pass validation for all fields populated', async () => {
     expect(config).toBeDefined()
   })
