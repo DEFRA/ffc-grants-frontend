bd9cfde8c298181094f8a65746df656f4c44bc08
ogbk <ogbu.k.olu@gmail.com>
2021-05-20 17:14:05 +0100

    cleanup maybe-eligible (use <button> not form)

--- app/routes/abstraction-caveat.js
+++ app/routes/abstraction-caveat.js
@@ -1,6 +1,6 @@
 const MAYBE_ELIGIBLE = {
   backLink: './abstraction-licence',
-  postUrl: './abstraction-caveat',
+  nextLink: './SSSI',
   messageHeader: 'You may be able to apply for this grant',
   messageContent: `
   Any abstraction licences should be in place by 31 December 2021 (the end of the application window).<br/><br/>
@@ -14,14 +14,7 @@ module.exports = [
     method: 'GET',
     path: '/abstraction-caveat',
     handler: (request, h) => {
-      return h.view('maybe-eligible-form', MAYBE_ELIGIBLE).takeover()
-    }
-  },
-  {
-    method: 'POST',
-    path: '/abstraction-caveat',
-    handler: (request, h) => {
-      return h.redirect('./SSSI')
+      return h.view('maybe-eligible', MAYBE_ELIGIBLE).takeover()
     }
   }
 ]
--- test/integration/narrow/routes/planning-caveat.test.js
+++ test/integration/narrow/routes/planning-caveat.test.js
@@ -1,4 +1,4 @@
-const { getCookieHeader, getCrumbCookie, crumbToken } = require('./test-helper')
+const { getCookieHeader, getCrumbCookie } = require('./test-helper')
 
 describe('Planning caveat page', () => {
   let crumCookie
@@ -15,18 +15,5 @@ describe('Planning caveat page', () => {
     expect(header.length).toBe(3)
     crumCookie = getCrumbCookie(response)
     expect(response.result).toContain(crumCookie[1])
-  })
-
-  it('redirects to abstraction licence page', async () => {
-    const postOptions = {
-      method: 'POST',
-      url: '/planning-caveat',
-      payload: { crumb: crumbToken },
-      headers: { cookie: 'crumb=' + crumbToken }
-    }
-
-    const postResponse = await global.__SERVER__.inject(postOptions)
-    expect(postResponse.statusCode).toBe(302)
-    expect(postResponse.headers.location).toBe('./abstraction-licence')
   })
 })
