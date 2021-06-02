ada6b34eec28abb9b777ea5d57c6feebeef9e719
diya2214 <madihakhn@yahoo.com>
2021-06-02 14:18:13 +0100

    agent Contact details

--- app/routes/agent-details.js
+++ app/routes/agent-details.js
@@ -1,14 +1,38 @@
 const Joi = require('joi')
 const { setYarValue, getYarValue } = require('../helpers/session')
 const { setLabelData, fetchListObjectItems, findErrorList } = require('../helpers/helper-functions')
-const { NAME_REGEX } = require('../helpers/regex-validation')
+const { NAME_REGEX, PHONE_REGEX, POSTCODE_REGEX, DELETE_POSTCODE_CHARS_REGEX } = require('../helpers/regex-validation')
+const { LIST_COUNTIES } = require('../helpers/all-counties')
 
 function createModel (errorMessageList, agentDetails) {
-  const { title, firstName, lastName } = agentDetails
+  const {
+    firstName,
+    lastName,
+    businessName,
+    email,
+    mobile,
+    landline,
+    address1,
+    address2,
+    town,
+    county,
+    postcode
+  } = agentDetails
 
-  const [titleError, firstNameError, lastNameError] = fetchListObjectItems(
+  const [
+    firstNameError,
+    lastNameError,
+    businessNameError,
+    emailError,
+    mobileError,
+    landlineError,
+    address1Error,
+    townError,
+    countyError,
+    postcodeError
+  ] = fetchListObjectItems(
     errorMessageList,
-    ['titleError', 'firstNameError', 'lastNameError']
+    ['firstNameError', 'lastNameError', 'businessNameError', 'emailError', 'mobileError', 'landlineError', 'address1Error', 'townError', 'countyError', 'postcodeError']
   )
 
   return {
@@ -16,16 +40,6 @@ function createModel (errorMessageList, agentDetails) {
     pageId: 'Agent',
     pageHeader: 'Agent\'s details',
     formActionPage: './agent-details',
-    selectTitle: {
-      id: 'title',
-      name: 'title',
-      classes: 'govuk-input--width-10',
-      label: {
-        text: 'Title (optional)'
-      },
-      items: setLabelData(title, [{ value: 'Other', text: 'Select an option' }, 'Mr', 'Mrs', 'Ms', 'Miss', 'Dr']),
-      ...(titleError ? { errorMessage: { text: titleError } } : {})
-    },
     inputFirstName: {
       id: 'firstName',
       name: 'firstName',
@@ -45,6 +59,102 @@ function createModel (errorMessageList, agentDetails) {
       },
       ...(lastName ? { value: lastName } : {}),
       ...(lastNameError ? { errorMessage: { text: lastNameError } } : {})
+    },
+    inputBusinessName: {
+      id: 'businessName',
+      name: 'businessName',
+      classes: 'govuk-input--width-20',
+      label: {
+        text: 'Business name'
+      },
+      ...(businessName ? { value: businessName } : {}),
+      ...(businessNameError ? { errorMessage: { text: businessNameError } } : {})
+    },
+    inputEmail: {
+      id: 'email',
+      name: 'email',
+      classes: 'govuk-input--width-20',
+      label: {
+        text: 'Email address'
+      },
+      hint: {
+        text:
+          'We will use this to send you a confirmation'
+      },
+      ...(email ? { value: email } : {}),
+      ...(emailError ? { errorMessage: { text: emailError } } : {})
+    },
+    inputMobile: {
+      id: 'mobile',
+      name: 'mobile',
+      classes: 'govuk-input--width-20',
+      label: {
+        text: 'Mobile number'
+      },
+      ...(mobile ? { value: mobile } : {}),
+      ...(mobileError ? { errorMessage: { text: mobileError } } : {})
+    },
+    inputLandline: {
+      id: 'landline',
+      name: 'landline',
+      classes: 'govuk-input--width-20',
+      label: {
+        text: 'Landline number (optional)'
+      },
+      ...(landline ? { value: landline } : {}),
+      ...(landlineError ? { errorMessage: { text: landlineError } } : {})
+    },
+    inputAddress1: {
+      id: 'address1',
+      name: 'address1',
+      classes: 'govuk-input--width-20',
+      label: {
+        text: 'Address 1'
+      },
+      ...(address1 ? { value: address1 } : {}),
+      ...(address1Error ? { errorMessage: { text: address1Error } } : {})
+    },
+    inputAddress2: {
+      id: 'address2',
+      name: 'address2',
+      classes: 'govuk-input--width-20',
+      label: {
+        text: 'Address 2 (optional)'
+      },
+      ...(address2 ? { value: address2 } : {})
+    },
+    inputTown: {
+      id: 'town',
+      name: 'town',
+      classes: 'govuk-input--width-10',
+      label: {
+        text: 'Town'
+      },
+      ...(town ? { value: town } : {}),
+      ...(townError ? { errorMessage: { text: townError } } : {})
+    },
+    selectCounty: {
+      id: 'county',
+      name: 'county',
+      classes: 'govuk-input--width-10',
+      label: {
+        text: 'County'
+      },
+      items: setLabelData(county, [
+        { text: 'Select an option', value: null },
+        ...LIST_COUNTIES
+      ]),
+      ...(countyError ? { errorMessage: { text: countyError } } : {})
+    },
+    inputPostcode: {
+      id: 'postcode',
+      name: 'postcode',
+      classes: 'govuk-input--width-5',
+      label: {
+        text: 'Postcode'
+      },
+      ...(postcode ? { value: postcode } : {}),
+      ...(postcodeError ? { errorMessage: { text: postcodeError } } : {})
     }
   }
 }
@@ -57,9 +167,17 @@ module.exports = [
       let agentDetails = getYarValue(request, 'agentDetails') || null
       if (!agentDetails) {
         agentDetails = {
-          title: 'Other',
           firstName: null,
-          lastName: null
+          lastName: null,
+          businessName: null,
+          email: null,
+          mobile: null,
+          landline: null,
+          address1: null,
+          address2: null,
+          town: null,
+          county: null,
+          postcode: null
         }
       }
       return h.view(
@@ -75,35 +193,52 @@ module.exports = [
       validate: {
         options: { abortEarly: false },
         payload: Joi.object({
-          title: Joi.any(),
           firstName: Joi.string().regex(NAME_REGEX).required(),
-          lastName: Joi.string().regex(NAME_REGEX).required()
+          lastName: Joi.string().regex(NAME_REGEX).required(),
+          businessName: Joi.string().max(100).required(),
+          email: Joi.string().email().required(),
+          mobile: Joi.string().regex(PHONE_REGEX).required(),
+          landline: Joi.string().regex(PHONE_REGEX).allow(''),
+          address1: Joi.string().required(),
+          address2: Joi.string().allow(''),
+          town: Joi.string().required(),
+          county: Joi.string().required(),
+          postcode: Joi.string().replace(DELETE_POSTCODE_CHARS_REGEX, '').regex(POSTCODE_REGEX).trim().required()
         }),
         failAction: (request, h, err) => {
           const [
-            titleError, firstNameError, lastNameError
-          ] = findErrorList(err, ['title', 'firstName', 'lastName'])
+            firstNameError,
+            lastNameError,
+            businessNameError,
+            emailError,
+            mobileError,
+            landlineError,
+            address1Error,
+            townError,
+            countyError,
+            postcodeError
+          ] = findErrorList(err, ['firstName', 'lastName', 'businessName', 'email', 'mobile', 'landline', 'address1', 'town', 'county', 'postcode'])
 
           const errorMessageList = {
-            titleError, firstNameError, lastNameError
+            firstNameError, lastNameError, businessNameError, emailError, mobileError, landlineError, address1Error, townError, countyError, postcodeError
           }
 
-          const { title, firstName, lastName } = request.payload
-          const agentDetails = { title, firstName, lastName }
+          const { firstName, lastName, businessName, email, mobile, landline, address1, address2, town, county, postcode } = request.payload
+          const agentDetails = { firstName, lastName, businessName, email, mobile, landline, address1, address2, town, county, postcode }
 
           return h.view('model-farmer-agent-details', createModel(errorMessageList, agentDetails)).takeover()
         }
       },
       handler: (request, h) => {
         const {
-          title, firstName, lastName
+          firstName, lastName, businessName, email, mobile, landline, address1, address2, town, county, postcode
         } = request.payload
 
         setYarValue(request, 'agentDetails', {
-          title, firstName, lastName
+          firstName, lastName, businessName, email, mobile, landline, address1, address2, town, county, postcode
         })
 
-        return h.redirect('./agent-contact-details')
+        return h.redirect('./farmer-details')
       }
     }
   }
