d841955b0e742b9cdbb125f90f2d105a729b3532
Madiha <45498508+diya2214@users.noreply.github.com>
2021-06-14 13:37:17 +0100

    FFTF-1086: Redirect all Eligibility question routes to project summary page if theres score  (#174)
    * redirect to project summary if theres score
    
    * update tests for project summary page guard

--- test/integration/narrow/routes/SSSI.test.js
+++ test/integration/narrow/routes/SSSI.test.js
@@ -1,34 +1,43 @@
 const { crumbToken } = require('./test-helper')
+const varListTemplate = {
+  farmingType: 'some fake crop',
+  legalStatus: 'fale status',
+  inEngland: 'Yes',
+  projectStarted: 'No',
+  landOwnership: 'Yes',
+  projectItemsList: {
+    projectEquipment: ['Boom', 'Trickle']
+  },
+  projectCost: '12345678',
+  remainingCost: 14082.00,
+  payRemainingCosts: 'Yes',
+  planningPermission: 'Will not have by 31 December 2021',
+  abstractionLicence: 'Not needed',
+  sSSI: 'Yes',
+  'current-score': ''
+
+}
+
+let varList
+const mockSession = {
+  setYarValue: (request, key, value) => null,
+  getYarValue: (request, key) => {
+    if (Object.keys(varList).includes(key)) return varList[key]
+    else return 'Error'
+  }
+}
+
+jest.mock('../../../../app/helpers/session', () => mockSession)
 
 describe('SSSI page', () => {
-  const varList = {
-    farmingType: 'some fake crop',
-    legalStatus: 'fale status',
-    inEngland: 'Yes',
-    projectStarted: 'No',
-    landOwnership: 'Yes',
-    projectItemsList: {
-      projectEquipment: ['Boom', 'Trickle']
-    },
-    projectCost: '12345678',
-    remainingCost: 14082.00,
-    payRemainingCosts: 'Yes',
-    planningPermission: 'Will not have by 31 December 2021',
-    abstractionLicence: 'Not needed',
-    sSSI: 'Yes'
-  }
-
-  jest.mock('../../../../app/helpers/session', () => ({
-    setYarValue: (request, key, value) => null,
-    getYarValue: (request, key) => {
-      if (Object.keys(varList).includes(key)) return varList[key]
-      else return 'Error'
-    }
-  }))
-
   beforeEach(() => {
-    jest.clearAllMocks()
+    varList = { ...varListTemplate }
   })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
   it('should load page successfully', async () => {
     const options = {
       method: 'GET',
@@ -37,6 +46,18 @@ describe('SSSI page', () => {
 
     const response = await global.__SERVER__.inject(options)
     expect(response.statusCode).toBe(200)
+  })
+
+  it('should redirect to project summary page when theres score', async () => {
+    varList['current-score'] = true
+    const options = {
+      method: 'GET',
+      url: `${global.__URLPREFIX__}/SSSI`
+    }
+
+    const response = await global.__SERVER__.inject(options)
+    expect(response.statusCode).toBe(302)
+    expect(response.headers.location).toBe(`${global.__URLPREFIX__}/project-summary`)
   })
 
   it('should return an error message if no option is selected', async () => {
