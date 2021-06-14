221457201326a620ffc862d012e470bec2b11416
Ellie Tahmasebi <59767576+ellietms@users.noreply.github.com>
2021-06-14 13:17:53 +0100

    FFTF-120: text update farming type (#173)
    * text update farming type

--- app/routes/farming-type.js
+++ app/routes/farming-type.js
@@ -34,7 +34,7 @@ function createModelNotEligible () {
   return {
     backLink: currentPath,
     messageContent:
-      'This grant is only available to<ul class="govuk-list govuk-list--bullet"><li> arable and horticultural farming businesses that supply the food industry</li><li>nurseries growing flowers</li><li>forestry nurseries</li></ul><p class="govuk-body"> <a href=\'https://www.gov.uk/topic/farming-food-grants-payments/rural-grants-payments\'>See other grants you may be eligible for.</a></p>'
+      'This grant is only available to<ul class="govuk-list govuk-list--bullet"><li> arable and horticultural farming businesses that supply the food industry</li><li>nurseries growing ornamentals</li><li>forestry nurseries</li></ul><p class="govuk-body"> <a href=\'https://www.gov.uk/topic/farming-food-grants-payments/rural-grants-payments\'>See other grants you may be eligible for.</a></p>'
   }
 }
 
