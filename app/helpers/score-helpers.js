eadaa53a6384a360a7d14f7f5534c17b42ae3b30
Madiha <45498508+diya2214@users.noreply.github.com>
2021-06-24 08:53:07 +0100

    FFTF-1242: Summary page heading fix and add irrigation status question (#186)
    * heading fix, 
    * added irrigation status question in score/summary page

---
+++ app/helpers/score-helpers.js
@@ -0,0 +1,23 @@
+
+function addSummaryRow (object, request) {
+  return {
+    key: object.key,
+    answers: [
+      {
+        key: object.key,
+        title: object.title,
+        input: [{ value: request.yar.get(object.yarKey) }]
+      }],
+    title: object.title,
+    desc: object.desc ?? '',
+    url: object.url,
+    order: object.order,
+    unit: object?.unit,
+    pageTitle: object.pageTitle,
+    fundingPriorities: object?.fundingPriorities
+  }
+}
+
+module.exports = {
+  addSummaryRow
+}
