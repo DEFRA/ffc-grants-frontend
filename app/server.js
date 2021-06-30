44d2fb38fcd0aa54a68f41359077646278f56da2
Madiha <45498508+diya2214@users.noreply.github.com>
2021-06-30 10:08:40 +0100

    Update server.js

--- app/server.js
+++ app/server.js
@@ -28,7 +28,7 @@ async function createServer () {
   await server.register(vision)
   await server.register(require('./plugins/cookies'))
   await server.register(require('./plugins/error-pages'))
-  // await server.register(require('./plugins/pageGuard'))
+  await server.register(require('./plugins/pageGuard'))
   await server.register({
     plugin: require('./plugins/header'),
     options: {
