0ca54492623c37233a3846d8b23c3d82eab9ea7c
Satish Chatap <satish.chatap@capgemini.com>
2021-07-22 22:38:05 +0100

    removed unwanted function

--- app/templates/components/timeout-warning/timeout-warning.js
+++ app/templates/components/timeout-warning/timeout-warning.js
@@ -1,4 +1,7 @@
 import dialogPolyfill from 'dialog-polyfill'
+const REDIRECT_MESSAGE = 'You are about to be redirected.'
+const EXTRA_TEXT = ' We do this to keep your information secure.'
+
 function TimeoutWarning ($module) {
   this.$module = $module
   this.$lastFocusedEl = null
@@ -114,93 +117,72 @@ TimeoutWarning.prototype.startUiCountdown = function () {
   const $countdown = this.$countdown
   const $accessibleCountdown = this.$accessibleCountdown
   const minutes = this.minutesTimeOutModalVisible
-  let timerRunOnce = false
+  const timerRunOnce = false
   const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream
   const timers = this.timers
 
-  let seconds = 60 * minutes
+  const seconds = 60 * minutes
 
-  $countdown.innerHTML = minutes + ' minute' + (minutes > 1 ? 's' : '');
-
-  (function runTimer () {
-    const minutesLeft = parseInt(seconds / 60, 10)
-    const secondsLeft = parseInt(seconds % 60, 10)
-    const timerExpired = minutesLeft < 1 && secondsLeft < 1
-    const minLeftText = (minutesLeft > 1 ? 's' : '')
-    const secLeftText = (secondsLeft > 1 ? 's' : '')
-    const minutesText = minutesLeft > 0 ? '<span class="tabular-numbers">' + minutesLeft + '</span> minute' + minLeftText + '' : ' '
-    const secondsText = secondsLeft >= 1 ? ' <span class="tabular-numbers">' + secondsLeft + '</span> second' + secLeftText + '' : ''
-    let atMinutesNumberAsText = ''
-    let atSecondsNumberAsText = ''
-
-    try {
-      atMinutesNumberAsText = this.numberToWords(minutesLeft) // Attempt to convert numerics into text as iOS VoiceOver ccassionally stalled when encountering numbers
-      atSecondsNumberAsText = this.numberToWords(secondsLeft)
-    } catch (e) {
-      atMinutesNumberAsText = minutesLeft
-      atSecondsNumberAsText = secondsLeft
-    }
-    const atMinutesText = minutesLeft > 0 ? atMinutesNumberAsText + ' minute' + minLeftText + '' : ''
-    const atSecondsText = secondsLeft >= 1 ? ' ' + atSecondsNumberAsText + ' second' + secLeftText + '' : ''
-
-    // Below string will get read out by screen readers every time the timeout refreshes (every 15 secs. See below).
-    // Please add additional information in the modal body content or in below extraText which will get announced to AT the first time the time out opens
-    const text = 'We will reset your application if you do not respond in ' + minutesText + secondsText + '.'
-    let atText = 'We will reset your application if you do not respond in ' + atMinutesText
-    if (atSecondsText) {
-      if (minutesLeft > 0) {
-        atText += ' and'
-      }
-      atText += atSecondsText + '.'
-    } else {
-      atText += '.'
-    }
-    const extraText = ' We do this to keep your information secure.'
-
-    if (timerExpired) {
-      // TO DO - client/server interaction
-      // GET last interactive time from server before timing out user
-      // to ensure that user hasn’t interacted with site in another tab
-
-      $countdown.innerHTML = 'You are about to be redirected.'
-      $accessibleCountdown.innerHTML = 'You are about to be redirected.'
-
-      setTimeout($module.redirect.bind($module), 4000)
-    } else {
-      seconds--
-
-      $countdown.innerHTML = text + extraText
-
-      if (minutesLeft < 1 && secondsLeft < 20) {
-        $accessibleCountdown.setAttribute('aria-live', 'assertive')
-      }
-
-      if (!timerRunOnce) {
-        // Read out the extra content only once. Don't read out on iOS VoiceOver which stalls on the longer text
-
-        if (iOS) {
-          $accessibleCountdown.innerHTML = atText
-        } else {
-          $accessibleCountdown.innerHTML = atText + extraText
-        }
-        timerRunOnce = true
-      } else if (secondsLeft % 15 === 0) {
-        // Update screen reader friendly content every 15 secs
-        $accessibleCountdown.innerHTML = atText
-      }
-
-      // TO DO - client/server interaction
-      // GET last interactive time from server while the warning is being displayed.
-      // If user interacts with site in second tab, warning should be dismissed.
-      // Compare what server returned to what is stored in client
-      // If needed, call this.closeDialog()
-
-      // JS doesn't allow resetting timers globally so timers need to be retained for resetting.
-      timers.push(setTimeout(runTimer, 1000))
-    }
-  })()
+  $countdown.innerHTML = minutes + ' minute' + (minutes > 1 ? 's' : '')
+  runTimer(seconds, timers, iOS, timerRunOnce, $accessibleCountdown, $module, $countdown)
 }
+function getSpanForNumber (time, isMinute) {
+  const pluralText = (time > 1 ? 's' : '')
+  const timeText = (isMinute === true ? 'minute' : 'second')
+  if (isMinute) {
+    return time > 0 ? '<span class="tabular-numbers">' + time + '</span> ' + timeText + pluralText + '' : ' '
+  } else {
+    return time >= 1 ? '<span class="tabular-numbers">' + time + '</span> ' + timeText + pluralText + '' : ' '
+  }
+}
+function getAtText (minLeftText, secondsLeft, secLeftText, minutesLeft) {
+  const atMinutesText = minutesLeft > 0 ? minutesLeft + ' minute' + minLeftText + '' : ''
+  const atSecondsText = secondsLeft >= 1 ? ' ' + secondsLeft + ' second' + secLeftText + '' : ''
+  let atText = 'We will reset your application if you do not respond in ' + atMinutesText
+  if (atSecondsText) {
+    if (minutesLeft > 0) {
+      atText += ' and'
+    }
+    atText += atSecondsText + '.'
+  } else {
+    atText += '.'
+  }
+  return atText
+}
+function runTimer (seconds, timers, iOS, timerRunOnce, $accessibleCountdown, $module, $countdown) {
+  const minutesLeft = parseInt(seconds / 60, 10)
+  const secondsLeft = parseInt(seconds % 60, 10)
+  const timerExpired = minutesLeft < 1 && secondsLeft < 1
+  const minLeftText = (minutesLeft > 1 ? 's' : '')
+  const secLeftText = (secondsLeft > 1 ? 's' : '')
+  const minutesText = getSpanForNumber(minutesLeft, true)
+  const secondsText = getSpanForNumber(secondsLeft, false)
+  // Below string will get read out by screen readers every time the timeout refreshes (every 15 secs. See below).
+  // Please add additional information in the modal body content or in below extraText which will get announced to AT the first time the time out opens
+  const text = 'We will reset your application if you do not respond in ' + minutesText + secondsText + '.'
+  const atText = getAtText(minLeftText, secondsLeft, secLeftText, minutesLeft)
 
+  if (timerExpired) {
+    $accessibleCountdown.innerHTML = $countdown.innerHTML = REDIRECT_MESSAGE
+    setTimeout($module.redirect.bind($module), 4000)
+  } else {
+    seconds--
+    $countdown.innerHTML = text + EXTRA_TEXT
+    if (minutesLeft < 1 && secondsLeft < 20) {
+      $accessibleCountdown.setAttribute('aria-live', 'assertive')
+    }
+    if (!timerRunOnce) {
+      // Read out the extra content only once. Don't read out on iOS VoiceOver which stalls on the longer text
+      $accessibleCountdown.innerHTML = iOS ? atText : (atText + EXTRA_TEXT)
+      timerRunOnce = true
+    } else if (secondsLeft % 15 === 0) {
+      // Update screen reader friendly content every 15 secs
+      $accessibleCountdown.innerHTML = atText
+    }
+    // JS doesn't allow resetting timers globally so timers need to be retained for resetting.
+    timers.push(setTimeout(() => runTimer(seconds, timers, iOS, timerRunOnce, $accessibleCountdown, $module, $countdown), 1000))
+  }
+}
 TimeoutWarning.prototype.saveLastFocusedEl = function () {
   this.$lastFocusedEl = document.activeElement
   if (!this.$lastFocusedEl || this.$lastFocusedEl === document.body) {
@@ -303,80 +285,6 @@ TimeoutWarning.prototype.redirect = function () {
 // Example function for sending last active time of user to server
 TimeoutWarning.prototype.setLastActiveTimeOnServer = function () {
   return 0
-}
-
-TimeoutWarning.prototype.numberToWords = function (n) {
-  const string = n.toString()
-  let end
-  let chunk
-  let ints
-  let i
-  let word
-
-  if (parseInt(string) === 0) {
-    return 'zero'
-  }
-
-  /* Array of units as words */
-  const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
-
-  /* Array of tens as words */
-  const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
-
-  /* Array of scales as words */
-  const scales = ['', 'thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion', 'decillion', 'undecillion', 'duodecillion', 'tredecillion', 'quatttuor-decillion', 'quindecillion', 'sexdecillion', 'septen-decillion', 'octodecillion', 'novemdecillion', 'vigintillion', 'centillion']
-
-  /* Split user arguemnt into 3 digit chunks from right to left */
-  let start = string.length
-  const chunks = []
-  while (start > 0) {
-    end = start
-    start = Math.max(0, start - 3)
-    chunks.push(string.slice(start, end))
-  }
-
-  /* Check if function has enough scale words to be able to stringify the user argument */
-  const chunksLen = chunks.length
-  if (chunksLen > scales.length) {
-    return ''
-  }
-
-  /* Stringify each integer in each chunk */
-  const words = []
-  for (i = 0; i < chunksLen; i++) {
-    chunk = parseInt(chunks[i])
-
-    if (chunk) {
-      /* Split chunk into array of individual integers */
-      ints = chunks[i].split('').reverse().map(parseFloat)
-
-      /* If tens integer is 1, i.e. 10, then add 10 to units integer */
-      if (ints[1] === 1) {
-        ints[0] += 10
-      }
-
-      /* Add scale word if chunk is not zero and array item exists */
-      if ((word === scales[i])) {
-        words.push(word)
-      }
-
-      /* Add unit word if array item exists */
-      if ((word === units[ints[0]])) {
-        words.push(word)
-      }
-
-      /* Add tens word if array item exists */
-      if ((word === tens[ints[1]])) {
-        words.push(word)
-      }
-
-      /* Add hundreds word if array item exists */
-      if ((word === units[ints[2]])) {
-        words.push(word + ' hundred')
-      }
-    }
-  }
-  return words.reverse().join(' ')
 }
 
 export default TimeoutWarning
