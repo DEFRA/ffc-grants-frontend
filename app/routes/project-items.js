809f0bfc69f5b0441873db8cac1cc9a99563979c
govTechSatish <80264942+govTechSatish@users.noreply.github.com>
2021-11-09 10:10:40 +0000

    Feature/waterstoragetank (#250)
    * Added water storage tank in project items.

--- app/routes/project-items.js
+++ app/routes/project-items.js
@@ -39,7 +39,8 @@ function createModel (errorList, backLink, projectInfrastucture, projectEquipmen
           'Pumphouse',
           'Underground water distribution main and hydrants',
           'Electricity installation for pumphouse',
-          'Water meter'
+          'Water meter',
+          'Water storage tanks'
         ]
       ),
       ...(errorList ? { errorMessage: { text: errorList[0].text } } : {})
