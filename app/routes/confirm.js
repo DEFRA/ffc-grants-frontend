80ae57f10511075307515a37a1be5c850beac6e6
sal.abuewilly@gmail.com <sal.abuewilly@gmail.com>
2022-09-12 22:03:56 +0100

    Update confirm and send page

--- app/routes/confirm.js
+++ app/routes/confirm.js
@@ -8,12 +8,29 @@ const currentPath = `${urlPrefix}/${viewTemplate}`
 const previousPath = `${urlPrefix}/check-details`
 const nextPath = `${urlPrefix}/confirmation`
 const startPath = startPageUrl
+const copyprovider = (key) => {
+  const copy = {
+    1: "Confirm and send",
+    2: "I confirm that, to the best of my knowledge, the details I have provided are correct.",
+    3: "I understand the project’s eligibility and score is basedon the answers I provided.",
+    4: "I am aware that the information I submit will be:",
+    5: "checked by the RPA",
+    6: "shared with the Environment Agency so that they can check the details of my planned project",
+    7: "I am happy to be contacted by Defra and RPA (or a third-party on their behalf) about my application.",
+    8: "So that we can continue to improve our services and schemes, we may wish to contact you in the future. Please confirm if you are happy for us, or a third-party working for us, to contact you.",
+    9: "(Optional) I consent to being contacted about improving services.",
+    10: "You can only submit your details once",
+    11: "Confirm and send",
+  }
+  return copy[ key ];
+}
 
 const CONSENT_OPTIONAL = 'CONSENT_OPTIONAL'
 
 function createModel (consentOptional, errorMessage) {
   return {
     backLink: previousPath,
+    copyprovider,
     formActionPage: currentPath,
     consentOptionalData: {
       idPrefix: 'consentOptional',
