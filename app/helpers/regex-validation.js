3f1d5da5cf04c206ed4941f970063593669dd258
Julian Taylor <julian.taylor@capgemini.com>
2022-09-20 17:15:20 +0100

    Brackets unneeded

--- app/helpers/regex-validation.js
+++ app/helpers/regex-validation.js
@@ -1,4 +1,4 @@
-const POSTCODE_REGEX = /^[\s]*[a-z]{1,2}\d[a-z\d]?[\s]*\d[a-z]{2}[\s]*$/i
+const POSTCODE_REGEX = /^\s*[a-z]{1,2}\d[a-z\d]?\s*\d[a-z]{2}\s*$/i
 const DELETE_POSTCODE_CHARS_REGEX = /[)(.\s-]*/g
 const PHONE_REGEX = /^[0-9\{\[\(\)\}\]— -+]+$/
 const NAME_REGEX = /^[a-zA-Z' -,’-]+$/
