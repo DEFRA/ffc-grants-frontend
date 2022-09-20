3f9af2c8e6401e52a4674228ebe271c5a51f9c6f
salfsdev <sal.abuewilly@gmail.com>
2022-09-20 19:37:37 +0100

    Update emailConfrim error copy

--- app/helpers/detailsInputs.js
+++ app/helpers/detailsInputs.js
@@ -7,84 +7,96 @@ const getDetailsInput = (detailsData, errorList) => {
   return {
     inputFirstName: formInputObject(
       'firstName', 'govuk-input--width-20', 'First name', null, {
-        fieldName: detailsData.firstName,
-        fieldError: getFieldError(errorList, '#firstName'),
-        inputType: 'text',
-        autocomplete: 'given-name'
-      }
+      fieldName: detailsData.firstName,
+      fieldError: getFieldError(errorList, '#firstName'),
+      inputType: 'text',
+      autocomplete: 'given-name'
+    }
     ),
 
     inputLastName: formInputObject(
       'lastName', 'govuk-input--width-20', 'Last name', null, {
-        fieldName: detailsData.lastName,
-        fieldError: getFieldError(errorList, '#lastName'),
-        inputType: 'text',
-        autocomplete: 'family-name'
-      }
+      fieldName: detailsData.lastName,
+      fieldError: getFieldError(errorList, '#lastName'),
+      inputType: 'text',
+      autocomplete: 'family-name'
+    }
     ),
 
     ...(Object.prototype.hasOwnProperty.call(detailsData, 'businessName') ? { inputBusinessName: formInputObject('businessName', 'govuk-input--width-20', 'Business name', null, { fieldName: detailsData.businessName, fieldError: getFieldError(errorList, '#businessName'), inputType: 'text', autocomplete: 'organization' }) } : null),
 
     inputTown: formInputObject(
       'town', 'govuk-input--width-10', 'Town', null, {
-        fieldName: detailsData.town,
-        fieldError: getFieldError(errorList, '#town'),
-        inputType: 'text',
-        autocomplete: 'address-level2'
-      }
+      fieldName: detailsData.town,
+      fieldError: getFieldError(errorList, '#town'),
+      inputType: 'text',
+      autocomplete: 'address-level2'
+    }
     ),
 
     inputAddress2: formInputObject(
       'address2', 'govuk-input--width-20', 'Address line 2 (optional)', null, {
-        fieldName: detailsData.address2,
-        fieldError: getFieldError(errorList, '#address2'),
-        inputType: 'text',
-        autocomplete: 'address-line2'
-      }
+      fieldName: detailsData.address2,
+      fieldError: getFieldError(errorList, '#address2'),
+      inputType: 'text',
+      autocomplete: 'address-line2'
+    }
     ),
 
     inputAddress1: formInputObject(
       'address1', 'govuk-input--width-20', 'Address line 1', null, {
-        fieldName: detailsData.address1,
-        fieldError: getFieldError(errorList, '#address1'),
-        inputType: 'text',
-        autocomplete: 'address-line1'
-      }
+      fieldName: detailsData.address1,
+      fieldError: getFieldError(errorList, '#address1'),
+      inputType: 'text',
+      autocomplete: 'address-line1'
+    }
     ),
     inputLandline: formInputObject(
       'landline', 'govuk-input--width-10', 'Landline number', 'We will only use this to contact you about your application', {
-        fieldName: detailsData.landline,
-        fieldError: getFieldError(errorList, '#landline'),
-        inputType: 'tel',
-        autocomplete: 'home tel'
-      }
+      fieldName: detailsData.landline,
+      fieldError: getFieldError(errorList, '#landline'),
+      inputType: 'tel',
+      autocomplete: 'home tel'
+    }
     ),
     inputMobile: formInputObject(
       'mobile', 'govuk-input--width-10', 'Mobile number', 'We will only use this to contact you about your application', {
-        fieldName: detailsData.mobile,
-        fieldError: getFieldError(errorList, '#mobile'),
-        inputType: 'tel',
-        autocomplete: 'mobile tel'
-      }
+      fieldName: detailsData.mobile,
+      fieldError: getFieldError(errorList, '#mobile'),
+      inputType: 'tel',
+      autocomplete: 'mobile tel'
+    }
     ),
     inputEmail: formInputObject(
       'email', 'govuk-input--width-20', 'Email address', 'We will use this to send you confirmation', {
-        fieldName: detailsData.email,
-        fieldError: getFieldError(errorList, '#email'),
-        inputType: 'email',
-        autocomplete: 'email'
-      }
+      fieldName: detailsData.email,
+      fieldError: getFieldError(errorList, '#email'),
+      inputType: 'email',
+      autocomplete: 'email'
+    }
     ),
 
-    ...(Object.prototype.hasOwnProperty.call(detailsData, 'emailConfirm') ? { inputEmailConfirm: formInputObject('emailConfirm', 'govuk-input--width-20', 'Confirm Email address', null, { fieldName: detailsData.emailConfirm, fieldError: getFieldError(errorList, '#emailConfirm'), inputType: 'email', autocomplete: 'email' }) } : {}),
+    ...(Object.prototype.hasOwnProperty.call(detailsData, 'emailConfirm') ?
+      {
+        inputEmailConfirm: formInputObject(
+          'emailConfirm', 'govuk-input--width-20', 'Confirm Email address', null, {
+          fieldName: detailsData.emailConfirm,
+          fieldError: getFieldError(errorList, '#emailConfirm'),
+          inputType: 'email',
+          autocomplete: 'email'
+        })
+      }
+      :
+      {}
+    ),
 
     inputPostcode: formInputObject(
       'postcode', 'govuk-input--width-5', 'Business postcode', null, {
-        fieldName: detailsData.postcode,
-        fieldError: getFieldError(errorList, '#postcode'),
-        inputType: 'text',
-        autocomplete: 'postal-code'
-      }
+      fieldName: detailsData.postcode,
+      fieldError: getFieldError(errorList, '#postcode'),
+      inputType: 'text',
+      autocomplete: 'postal-code'
+    }
     ),
 
     ...(Object.prototype.hasOwnProperty.call(detailsData, 'projectPostcode') ? { inputProjPostcode: formInputObject('projectPostcode', 'govuk-input--width-5', 'Project postcode', 'The site postcode where the work will happen', { fieldName: detailsData.projectPostcode, fieldError: getFieldError(errorList, '#projectPostcode'), inputType: 'text', autocomplete: 'organization' }) } : {}),
--- app/helpers/lookupErrorText.js
+++ app/helpers/lookupErrorText.js
@@ -54,7 +54,7 @@ const errors = {
   'error.email.any.required': 'Enter your email address',
   'error.email.string.email': 'Enter an email address in the correct format, like name@example.com',
   'error.emailConfirm.string.email': 'Enter an email address in the correct format, like name@example.com',
-  'error.emailConfirm.string.empty': 'Enter an email address that matches',
+  'error.emailConfirm.string.empty': 'Confirm your email address',
   'error.emailConfirm.any.required': 'Enter an email address that matches',
   'error.mobile.string.empty': 'Enter a mobile number (If you do not have a mobile, enter your landline number)',
   'error.mobile.any.required': 'Enter a mobile number (If you do not have a mobile, enter your landline number)',
