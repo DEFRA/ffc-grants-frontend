45a82ad5dd2d29974a6d2bfaf11afc9caa8fad8d
zavies97 <zacsdavies@sky.com>
2022-09-27 11:14:29 +0100

    Updated agent details and farmer details
    error messages added to farmer details had not been added to agent details. Has now been updated accordingly

--- app/helpers/regex-validation.js
+++ app/helpers/regex-validation.js
@@ -1,12 +1,13 @@
 const POSTCODE_REGEX = /^\s*[a-z]{1,2}\d[a-z\d]?\s*\d[a-z]{2}\s*$/i
 const DELETE_POSTCODE_CHARS_REGEX = /[)(.\s-]*/g
-const PHONE_REGEX = /^[0-9\{\[\(\)\}\]— +]+$/
+const PHONE_REGEX = /^\+?[0-9\[\s\(\)\]\-]{10,}$/
 const NAME_REGEX = /^[a-zA-Z' ,’-]+$/
 const BUSINESSNAME_REGEX = /^[a-zA-Z0-9' ,’-]+$/
 const NUMBER_REGEX = /^\d+$/
 const PROJECT_COST_REGEX = /^[1-9]\d*$/
 const IRRIGATED_LAND_REGEX = /^(\d+([.]\d?)?|[.]\d)$/
 const ONLY_ZEROES_REGEX = /^0+$/
+const ADDRESS_REGEX = /^[a-zA-Z0-9' -]*$/
 const TOWN_REGEX = /^[a-zA-Z -]+$/
 
 module.exports = {
@@ -19,5 +20,6 @@ module.exports = {
   PROJECT_COST_REGEX,
   IRRIGATED_LAND_REGEX,
   ONLY_ZEROES_REGEX,
-  TOWN_REGEX
+  TOWN_REGEX,
+  ADDRESS_REGEX
 }
