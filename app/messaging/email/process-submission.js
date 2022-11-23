4ac4e586f9b15710bfb699a6334f97b1be778f79
sal.abuewilly@gmail.com <sal.abuewilly@gmail.com>
2022-11-23 14:38:29 +0000

    Process subbmissions and tests

--- app/messaging/email/process-submission.js
+++ app/messaging/email/process-submission.js
@@ -8,13 +8,11 @@ module.exports = async function (msg) {
     } = msg
     console.log('overAllScore - proc subb: ', overAllScore);
     console.log('correlationId - proc sub: ', correlationId);
-    // Get details from cache regarding desirability score
-    // const desirabilityScore = await cache.getDesirabilityScore(correlationId)
 
     console.log('MADE IT TO DETAILS', submissionDetails)
     const msgOut = createMsg(submissionDetails, overAllScore)
     console.log('msgOut - proc sub: ', msgOut);
-    // await sendDesirabilitySubmitted(msgOut, correlationId)
+
     return msgOut
 
   } catch (err) {
