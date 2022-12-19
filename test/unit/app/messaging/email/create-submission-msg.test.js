26fa10060c4c60da788cc5b1838b81b78ff310e6
diya2214 <madihakhn@yahoo.com>
2022-12-19 15:37:31 +0000

    test fix

--- test/unit/app/messaging/email/create-submission-msg.test.js
+++ test/unit/app/messaging/email/create-submission-msg.test.js
@@ -32,7 +32,7 @@ describe('Create submission message', () => {
     expect(msg).toHaveProperty('applicantEmail')
     expect(msg).toHaveProperty('rpaEmail')
     expect(msg).toHaveProperty('spreadsheet')
-    expect(msg.applicantEmail.emailAddress).toBe(farmerSubmission.farmerDetails.email)
+    expect(msg.applicantEmail.emailAddress).toBe(farmerSubmission.farmerDetails.emailAddress)
     expect(msg.rpaEmail.emailAddress).toBe('FTF@rpa.gov.uk')
     expect(msg.agentEmail).toBe(null)
   })
@@ -49,7 +49,7 @@ describe('Create submission message', () => {
     expect(msg).toHaveProperty('applicantEmail')
     expect(msg).toHaveProperty('rpaEmail')
     expect(msg).toHaveProperty('spreadsheet')
-    expect(msg.applicantEmail.emailAddress).toBe(farmerSubmission.farmerDetails.email)
+    expect(msg.applicantEmail.emailAddress).toBe(farmerSubmission.farmerDetails.emailAddress)
     expect(msg.rpaEmail.emailAddress).toBeFalsy
     expect(msg.agentEmail).toBe(null)
   })
@@ -67,8 +67,8 @@ describe('Create submission message', () => {
     expect(msg).toHaveProperty('applicantEmail')
     expect(msg).toHaveProperty('rpaEmail')
     expect(msg).toHaveProperty('spreadsheet')
-    expect(msg.agentEmail.emailAddress).toBe(agentSubmission.agentDetails.email)
-    expect(msg.applicantEmail.emailAddress).toBe(agentSubmission.farmerDetails.email)
+    expect(msg.agentEmail.emailAddress).toBe(agentSubmission.agentDetails.emailAddress)
+    expect(msg.applicantEmail.emailAddress).toBe(agentSubmission.farmerDetails.emailAddress)
     expect(msg.rpaEmail.emailAddress).toBe('FTF@rpa.gov.uk')
 
   })
