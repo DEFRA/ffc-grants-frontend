d5d23acc6871999ef8b76d4db4d21760e4b99577
zavies97 <zacsdavies@sky.com>
2023-01-06 13:18:15 +0000

    updated abstraction caveat test

--- test/integration/narrow/routes/abstraction-caveat.test.js
+++ test/integration/narrow/routes/abstraction-caveat.test.js
@@ -1,9 +1,10 @@
 
 describe('Abstraction caveat page', () => {
-  it('should load page successfully', async () => {
+
+  it('should load page successfully (abstraction)', async () => {
     const options = {
       method: 'GET',
-      url: `${global.__URLPREFIX__}/planning-required-condition`
+      url: `${global.__URLPREFIX__}/abstraction-required-condition`
     }
 
     const response = await global.__SERVER__.inject(options)
