70c89689e7f14629181324e0518eb1604c532299
diya2214 <madihakhn@yahoo.com>
2023-01-17 23:16:42 +0000

    water source data

---
+++ app/helpers/water-source-data.js
@@ -0,0 +1,13 @@
+const DONT_USE = ['Winter peak-flow abstraction', 'Rain water harvesting', 'Bore hole/aquifer']
+const DECREASE = ['Summer water surface abstraction', 'Mains', 'Winter peak-flow abstraction', 'Rain water harvesting', 'Bore hole/aquifer']
+const STOP_MAINTAIN_INTRODUCE_CURRENT = ['Summer water surface abstraction', 'Mains', 'Winter peak-flow abstraction', 'Rain water harvesting', 'Bore hole/aquifer']
+const STOP_MAINTAIN_INTRODUCE_PLANNED = ['Winter peak-flow abstraction', 'Rain water harvesting', 'Bore hole/aquifer']
+const MAINTAIN = ['Summer water surface abstraction', 'Mains']
+
+module.exports = {
+  DONT_USE,
+  DECREASE,
+  STOP_MAINTAIN_INTRODUCE_CURRENT,
+  STOP_MAINTAIN_INTRODUCE_PLANNED,
+  MAINTAIN
+}
