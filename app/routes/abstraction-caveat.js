05edfe0735cd9afd63bcb2c5516a77fef192ce3b
zavies97 <zavies97@users.noreply.github.com>
2023-01-18 15:22:04 +0000

    fixed grants 1564 (#360)
    * fixed grants 1564, 1566, 1567, 1576, 1565

--- app/routes/abstraction-caveat.js
+++ app/routes/abstraction-caveat.js
@@ -2,7 +2,7 @@ const urlPrefix = require('../config/server').urlPrefix
 
 const MAYBE_ELIGIBLE = {
   backLink: `${urlPrefix}/abstraction-licence`,
-  nextLink: `${urlPrefix}/project-summary`,
+  nextUrl: `${urlPrefix}/project-summary`,
   messageHeader: 'You may be able to apply for this grant',
   messageContent: `
   Any abstraction licences or variations must be in place by 31 January 2023.
