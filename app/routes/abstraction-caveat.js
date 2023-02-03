fbea36a4b9d898214ef35b93c3b442a6c825597b
yunusfirat <yunusfirat44@gmail.com>
2023-02-03 16:43:24 +0000

    Revert "abstraction licence condition date updates."
    This reverts commit 247dc8760e342b8451fc6e1ec301522994cb7190.

--- app/routes/abstraction-caveat.js
+++ app/routes/abstraction-caveat.js
@@ -5,7 +5,7 @@ const MAYBE_ELIGIBLE = {
   nextUrl: `${urlPrefix}/project-summary`,
   messageHeader: 'You may be able to apply for this grant',
   messageContent: `
-  Any abstraction licences or variations must be in place by 31 January 2024.
+  Any abstraction licences or variations must be in place by 31 January 2023.
   <div class="govuk-inset-text">If you have already applied for an abstraction licence or variation and expect to get it by then, you can submit a full application.</div>
   <p class="govuk-body">If you have already applied for an abstraction licence or variation but it might not be issued by then, please email <a class="govuk-link" href="mailto:FTF@rpa.gov.uk">FTF@rpa.gov.uk</a></p>
   
