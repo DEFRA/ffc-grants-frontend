bac6d7e210f40dc269199624f163796f611b8a99
zavies97 <zacsdavies@sky.com>
2023-02-06 11:00:10 +0000

    added abstraction-required-condition test

---
+++ test/integration/narrow/routes/abstraction-required-condition.test.js
@@ -0,0 +1,12 @@
+
+describe('Abstraction required condition page', () => {
+    it('should load page successfully', async () => {
+        const options = {
+            method: 'GET',
+            url: `${global.__URLPREFIX__}/abstraction-required-condition`
+        }
+
+        const response = await global.__SERVER__.inject(options)
+        expect(response.statusCode).toBe(200)
+    })
+})
