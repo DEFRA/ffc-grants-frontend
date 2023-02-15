7ae7e7d5820232981fa06f327579647efe0be8b5
diya2214 <madihakhn@yahoo.com>
2023-02-15 17:18:26 +0000

    water source data

--- app/helpers/water-source-data.js
+++ app/helpers/water-source-data.js
@@ -1,9 +1,13 @@
 const WATER_SOURCE = ['Summer water surface abstraction', 'Mains', 'Winter peak-flow abstraction', 'Rain water harvesting', 'Bore hole/aquifer']
 const SUSTAINABLE_WATER_SOURCE = ['Winter peak-flow abstraction', 'Rain water harvesting', 'Bore hole/aquifer']
 const UNSUSTAINABLE_WATER_SOURCE = ['Summer water surface abstraction', 'Mains']
+const SUMMER_ABSTRACTION_MAINS_YES = 'increase your use of'
+const SUMMER_ABSTRACTION_MAINS_NO = 'use water from'
 
 module.exports = {
   WATER_SOURCE,
   SUSTAINABLE_WATER_SOURCE,
-  UNSUSTAINABLE_WATER_SOURCE
+  UNSUSTAINABLE_WATER_SOURCE,
+  SUMMER_ABSTRACTION_MAINS_YES,
+  SUMMER_ABSTRACTION_MAINS_NO
 }
