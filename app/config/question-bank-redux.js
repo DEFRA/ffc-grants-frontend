f5a1936d10594f0e5df3b0cac94dc40924881472
zavies97 <zavies97@users.noreply.github.com>
2023-02-15 13:06:08 +0000

    updated for PR comments

--- app/config/question-bank-redux.js
+++ app/config/question-bank-redux.js
@@ -792,7 +792,7 @@ const questionBank = {
       type: 'single-answer',
       minAnswerCount: 1,
       ineligibleContent: {
-        messageContent: `Your project cannot increase water use from summer water surface abstraction or mains. 
+        messageContent: `Your project cannot increase water use from summer abstraction or mains. 
                           <div class='govuk-inset-text'>
                           RPA wants to fund projects that use more sustainable water sources, such as:</br></br>
                             <ul class='govuk-list govuk-list--bullet'>
@@ -830,7 +830,7 @@ const questionBank = {
             heading: 'Eligibility',
             content: [
               {
-                para: `Your project cannot increase water use from summer water surface abstraction or mains.\n\n
+                para: `Your project cannot increase water use from summer abstraction or mains.\n\n
                   RPA wants to fund projects that use more sustainable water sources, such as: `,
                 items: [
                   'winter peak-flow abstraction',
