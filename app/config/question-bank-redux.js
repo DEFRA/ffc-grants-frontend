962ecd8fdc23ae3b5197a646e3f32176eba77b72
Yunus FIRAT <yunusfirat44@gmail.com>
2023-02-16 21:18:14 +0000

    revert changes

--- app/config/question-bank-redux.js
+++ app/config/question-bank-redux.js
@@ -811,8 +811,8 @@ const questionBank = {
         {
           type: 'NOT_EMPTY',
           dependentYarKey:'currentlyIrrigating',
-          error:'Select yes if you’re increasing use of water from summer water surface abstraction or mains',
-          dependantError:'Select yes if you’re going to use summer water surface abstraction or mains'
+          error:'Select yes if you’re increasing use of water from summer water surface abstraction or main',
+          dependantError:'Select yes if you’re going to use summer abstraction or mains'
             
         }
       ],
