b5180f810080a03efa986003ff6a9b90802d2e47
diya2214 <madihakhn@yahoo.com>
2023-03-15 09:40:43 +0000

    abstraction license dates

--- app/routes/abstraction-caveat.js
+++ app/routes/abstraction-caveat.js
@@ -5,7 +5,7 @@ const MAYBE_ELIGIBLE = {
   nextUrl: `${urlPrefix}/irrigation-status`,
   messageHeader: 'You may be able to apply for a grant from this scheme',
   messageContent: `
-  Any abstraction licences or variations must be in place by 31 January 2024.
+  Any abstraction licences or variations must be in place by 31 October 2024.
   <div class="govuk-inset-text">If you have already applied for an abstraction licence or variation and expect to get it by then, you can submit a full application.</div>
   <p class="govuk-body">If you have already applied for an abstraction licence or variation but it might not be issued by then, please email <a class="govuk-link" href="mailto:FTF@rpa.gov.uk">FTF@rpa.gov.uk</a></p>
   
