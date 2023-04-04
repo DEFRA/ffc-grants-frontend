8bc86897c3ff2cd29428c3ef006d5e9b388e2889
diya2214 <madihakhn@yahoo.com>
2023-04-04 17:34:56 +0100

    process ga

--- app/helpers/handlers.js
+++ app/helpers/handlers.js
@@ -125,6 +125,8 @@ const getPage = async (question, request, h) => {
     )
   }
 
+  await processGA(question, request, confirmationId)
+
   switch (url) {
     case 'check-details': {
       return h.view('check-details', getCheckDetailsModel(request, question, backUrl, nextUrl))
@@ -233,6 +235,12 @@ const getHandler = (question) => {
 const getPostHandler = (currentQuestion) => {
   return (request, h) => {
     return showPostPage(currentQuestion, request, h)
+  }
+}
+
+const processGA = async (question, request, confirmationId) => {
+  if (question.ga) {
+    await gapiService.processGA(request, question.ga, confirmationId)
   }
 }
 
