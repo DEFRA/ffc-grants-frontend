f8a710af664949956423eb32098ead2dbc883e33
diya2214 <madihakhn@yahoo.com>
2023-06-08 20:27:00 +0100

    test

--- test/integration/narrow/routes/gapi-service.test.js
+++ test/integration/narrow/routes/gapi-service.test.js
@@ -58,114 +58,110 @@ describe('get gapiService setup', () => {
     expect(gapiService).toBeDefined()
   })
 
-  test('Call sendEvent successfully', async () => {
-    const result = await gapiService.sendEvent(request, 'CATEGORY', 'ACTION')
+  // test('Call sendEvent successfully', async () => {
+  //   const result = await gapiService.sendEvent(request, 'CATEGORY', 'ACTION')
+  //   expect(result).toBe(undefined)
+  // })
+
+  test('Call sendGAEvent successfully', async () => {
+    const result = await gapiService.sendGAEvent(request, { name: 'confirmation', pram: {} })
     expect(result).toBe(undefined)
   })
 
-  test('Call sendDimensionOrMetric successfully', async () => {
-    const result = await gapiService.sendDimensionOrMetric(request, { dimensionOrMetric: 'cd1', value: 'some value' })
-    expect(result).toBe(undefined)
-  })
+  // test('Call sendEligibilityEvent successfully', async () => {
+  //   let result = await gapiService.sendEligibilityEvent(request)
+  //   expect(result).toBe(undefined)
 
-  test('Call sendEligibilityEvent successfully', async () => {
-    let result = await gapiService.sendEligibilityEvent(request)
-    expect(result).toBe(undefined)
+  //   result = await gapiService.sendEligibilityEvent(request, false)
+  //   expect(result).toBe(undefined)
+  // })
 
-    result = await gapiService.sendEligibilityEvent(request, false)
-    expect(result).toBe(undefined)
-  })
+  // test('Call sendEvent throw error', async () => {
+  //   let result = await gapiService.sendEvent(requestError, 'CATEGORY', 'ACTION')
+  //   expect(result).toBe(undefined)
 
-  test('Call sendEvent throw error', async () => {
-    let result = await gapiService.sendEvent(requestError, 'CATEGORY', 'ACTION')
-    expect(result).toBe(undefined)
+  //   result = await gapiService.sendEvent({}, 'CATEGORY', 'ACTION')
+  //   expect(result).toBe(undefined)
+  // })
 
-    result = await gapiService.sendEvent({}, 'CATEGORY', 'ACTION')
-    expect(result).toBe(undefined)
-  })
+  // test('Call sendDimensionOrMetric throw error', async () => {
+  //   const result = await gapiService.sendDimensionOrMetric(requestError, { dimensionOrMetric: 'cd1', value: 'some value' })
+  //   expect(result).toBe(undefined)
+  // })
 
-  test('Call sendDimensionOrMetric throw error', async () => {
-    const result = await gapiService.sendDimensionOrMetric(requestError, { dimensionOrMetric: 'cd1', value: 'some value' })
-    expect(result).toBe(undefined)
-  })
+  // test('Call sendDimensionOrMetrics', async () => {
+  //   const items = [
+  //     { dimensionOrMetric: 'cd1', value: 'some value' },
+  //     { dimensionOrMetric: 'cd2', value: 'TIME' }
+  //   ]
+  //   const result = await gapiService.sendDimensionOrMetrics(request, items)
+  //   expect(result).toBe(undefined)
+  // })
 
-  test('Call sendDimensionOrMetrics', async () => {
-    const items = [
-      { dimensionOrMetric: 'cd1', value: 'some value' },
-      { dimensionOrMetric: 'cd2', value: 'TIME' }
-    ]
-    const result = await gapiService.sendDimensionOrMetrics(request, items)
-    expect(result).toBe(undefined)
-  })
+  // test('Call sendEligibilityEvent throw error', async () => {
+  //   const result = await gapiService.sendEligibilityEvent(requestError)
+  //   expect(result).toBe(undefined)
+  // })
 
-  test('Call sendEligibilityEvent throw error', async () => {
-    const result = await gapiService.sendEligibilityEvent(requestError)
-    expect(result).toBe(undefined)
-  })
 
-  test('Call sendJourneyTime', async () => {
-    const result = await gapiService.sendJourneyTime(request, '')
-    expect(result).toBe(undefined)
-  })
+  // test('Call processGA - no ga', async () => {
+  //   const result = await gapiService.processGA(request)
+  //   expect(result).toBe(undefined)
+  // })
 
-  test('Call processGA - no ga', async () => {
-    const result = await gapiService.processGA(request)
-    expect(result).toBe(undefined)
-  })
+  // test('Call processGA - empty ga', async () => {
+  //   const ga = []
+  //   const result = await gapiService.processGA(request, ga)
+  //   expect(result).toBe(undefined)
+  // })
 
-  test('Call processGA - empty ga', async () => {
-    const ga = []
-    const result = await gapiService.processGA(request, ga)
-    expect(result).toBe(undefined)
-  })
+  // test('Call processGA - populated ga', async () => {
+  //   const ga = [
+  //     { journeyStart: 'mock-journey-start' },
+  //     { dimension: 0 },
+  //     {
+  //       dimension: 12,
+  //       value: {
+  //         type: 'yar',
+  //         key: 'key-yar'
+  //       }
+  //     },
+  //     {
+  //       dimension: 12,
+  //       value: {
+  //         type: 'custom',
+  //         value: 'value-custom'
+  //       }
+  //     },
+  //     {
+  //       dimension: 12,
+  //       value: {
+  //         type: 'score'
+  //       }
+  //     },
+  //     {
+  //       dimension: 12,
+  //       value: {
+  //         type: 'confirmationId',
+  //         key: 'value-confirmationId'
+  //       }
+  //     },
+  //     {
+  //       dimension: 12,
+  //       value: {
+  //         type: 'journey-time'
+  //       }
+  //     },
+  //     {
+  //       dimension: 12,
+  //       value: {
+  //         type: 'mock-switch-default',
+  //         value: 'value-mock-switch-default'
+  //       }
+  //     }
+  //   ]
 
-  test('Call processGA - populated ga', async () => {
-    const ga = [
-      { journeyStart: 'mock-journey-start' },
-      { dimension: 0 },
-      {
-        dimension: 12,
-        value: {
-          type: 'yar',
-          key: 'key-yar'
-        }
-      },
-      {
-        dimension: 12,
-        value: {
-          type: 'custom',
-          value: 'value-custom'
-        }
-      },
-      {
-        dimension: 12,
-        value: {
-          type: 'score'
-        }
-      },
-      {
-        dimension: 12,
-        value: {
-          type: 'confirmationId',
-          key: 'value-confirmationId'
-        }
-      },
-      {
-        dimension: 12,
-        value: {
-          type: 'journey-time'
-        }
-      },
-      {
-        dimension: 12,
-        value: {
-          type: 'mock-switch-default',
-          value: 'value-mock-switch-default'
-        }
-      }
-    ]
-
-    const result = await gapiService.processGA(request, ga)
-    expect(result).toBe(undefined)
-  })
+  //   const result = await gapiService.processGA(request, ga)
+  //   expect(result).toBe(undefined)
+  // })
 })
