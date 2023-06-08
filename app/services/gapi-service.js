600e9593c7f4ab216994314cf63e65c8ab0f5f74
diya2214 <madihakhn@yahoo.com>
2023-06-08 15:46:25 +0100

    eligibility ga

--- app/services/gapi-service.js
+++ app/services/gapi-service.js
@@ -13,10 +13,10 @@ const grant_type = 'Water Management'
 const eventTypes = {
   PAGEVIEW: 'pageview',
   SCORE: 'score',
-  ELIGIBILITY: 'eligibility',
+  ELIGIBILITY: 'eligibility_passed',
   CONFIRMATION: 'confirmation',
   ELIMINATION: 'elimination',
-  EXCEPTION: 'exception',
+  EXCEPTION: 'exception'
 }
 
 const sendGAEvent = async (request, metrics) => {
@@ -24,20 +24,18 @@ const sendGAEvent = async (request, metrics) => {
   const pagePath = request.route.path
   const { name, params } = metrics
   const isEliminationEvent = name === eventTypes.ELIMINATION
+  const isEligibilityEvent = name === eventTypes.ELIGIBILITY
   const isScoreEvent = name === eventTypes.SCORE
   const isConfirmationEvent = name === eventTypes.CONFIRMATION
   const dmetrics = {
     ...params,
-    ...(isEliminationEvent && { elimination_time: '00:00:50' }),
+    ...(isEliminationEvent && { elimination_time: timeSinceStart }),
+    ...(isEligibilityEvent && { eligibility_time: timeSinceStart }),
     ...(isScoreEvent && { score_time: timeSinceStart }),
-    ...(isConfirmationEvent && { confirmation_time: timeSinceStart }),
-    ...(params.score_presented && { score_presented: params.score_presented }),
-    ...(params.final_score && { final_score: params.final_score }),
-    ...(params.user_type && { user_type: params.user_type }),
+    ...(isConfirmationEvent && { final_score: getYarValue(request, 'current-score'), user_type: getYarValue(request, 'applying'), confirmation_time: timeSinceStart }),
+    ...(params?.score_presented && { score_presented: params.score_presented }),
     grant_type,
-    pagePath,
-    page_title: pagePath,
-    test: 'testig v.1.6'
+    page_title: pagePath
   }
   try {
     console.log('dmetrics:::::: ', dmetrics)
@@ -50,55 +48,11 @@ const sendGAEvent = async (request, metrics) => {
   console.log('[ %s MATRIC SENT ]', name.toUpperCase())
 }
 
-
 const getTimeofJourneySinceStart = (request) => {
   if (getYarValue(request, 'journey-start-time')) {
     return Math.abs(((new Date()).getTime() - (new Date(getYarValue(request, 'journey-start-time'))).getTime()) / 1000)
   }
   return 0
-}
-
-const processGA = async (request, ga, _score, _confirmationId) => {
-  if (ga && Array.isArray(ga)) {
-    const cmcds = []
-    ga.forEach(async gaConfig => {
-      if (gaConfig.journeyStart) {
-        setYarValue(request, 'journey-start-time', Date.now())
-        console.log('[JOURNEY STARTED] ')
-      }
-      if (gaConfig.dimension) {
-        let value
-        switch (gaConfig.value.type) {
-          case 'yar':
-            value = getYarValue(request, gaConfig.value.key)
-            break
-          case 'custom':
-            value = gaConfig.value.value
-            break
-          case 'score':
-            value = gaConfig.value.value
-            break
-          case 'confirmationId':
-            await sendMonitoringEvent(request, request.yar.id, 'FTF-JOURNEY-COMPLETED', '0706')
-            value = gaConfig.value.value
-            break
-          case 'journey-time':
-            value = getTimeofJourneySinceStart(request).toString()
-            break
-          default:
-            value = gaConfig.value.value
-        }
-
-        cmcds.push({
-          dimensionOrMetric: gaConfig.dimension,
-          value: value?.toString()
-        })
-      }
-    })
-    if (cmcds.length > 0) {
-      // await sendDimensionOrMetrics(request, cmcds)
-    }
-  }
 }
 
 module.exports = {
