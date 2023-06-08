f306d25a763b0626ce51da43dd913300300eb4b3
diya2214 <madihakhn@yahoo.com>
2023-06-08 20:26:49 +0100

    remove log

--- app/plugins/gapi.js
+++ app/plugins/gapi.js
@@ -19,7 +19,6 @@ exports.plugin = {
         const statusFamily = Math.floor(response.statusCode / 100)
         if (statusFamily === 2 && response.variety === 'view' && !gapiService.isBlockDefaultPageView(request.url)) {
           await gapiService.sendGAEvent(request, { name: gapiService.eventTypes.PAGEVIEW, params: { page_path: request.route.path, page_title: request.route.fingerprint } })
-          console.log('NOT: from plugin Metrics Sending analytics page-view for %s', request.route.path)
         }
         if (statusFamily === 5) {
           await request.ga.event({ category: 'Exception', action: request.route.path, label: response.statusCode })
