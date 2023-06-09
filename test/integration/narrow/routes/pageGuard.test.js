7ea7af7bcf6be768cb5c6ea4a9d01b80b5c2e9fb
diya2214 <madihakhn@yahoo.com>
2023-06-09 10:35:36 +0100

    page guard test

--- test/integration/narrow/routes/pageGuard.test.js
+++ test/integration/narrow/routes/pageGuard.test.js
@@ -1,9 +1,7 @@
-const createServer = require('../../../../app/server')
 const { startPageUrl } = require('../../../../app/config/server')
 
 describe('Page Guard', () => {
   const OLD_ENV = process.env
-  let server
 
   beforeEach(async () => {
     jest.resetModules()
@@ -12,19 +10,17 @@ describe('Page Guard', () => {
 
   afterEach(() => {
     process.env = OLD_ENV
-    server.stop()
   })
 
   it('shoud redirect to start page if the site is decommissioned', async () => {
-    process.env.SERVICE_END_DATE = '2025/01/12'
+    process.env.SERVICE_END_DATE = '2020/01/12'
     process.env.SERVICE_END_TIME = '23:59:58'
-    server = await createServer()
     const getOptions = {
       method: 'GET',
       url: `${global.__URLPREFIX__}/legal-status`
     }
 
-    const getResponse = await server.inject(getOptions)
+    const getResponse = await global.__SERVER__.inject(getOptions)
     expect(getResponse.statusCode).toBe(302)
     expect(getResponse.headers.location).toBe(startPageUrl)
   })
