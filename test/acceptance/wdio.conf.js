809ab842d79b496c655a1f23d068da1a8da8ecac
zavies97 <zavies97@users.noreply.github.com>
2023-08-30 15:07:29 +0100

    Fix/security issues (#441)
    * Fix audit issues
    
    * version up

--- test/acceptance/wdio.conf.js
+++ test/acceptance/wdio.conf.js
@@ -1,108 +1,38 @@
-const browserstack = require('browserstack-local')
 const { ReportAggregator, HtmlReporter } = require('@rpii/wdio-html-reporter')
 const log4js = require('@log4js-node/log4js-api')
 const logger = log4js.getLogger('default')
-// const envRoot = (process.env.TEST_ENVIRONMENT_ROOT_URL || 'https://ffc-grants-frontend.ffc.snd.azure.defra.cloud/water/start')
-// const envRoot = (process.env.TEST_ENVIRONMENT_ROOT_URL || 'https://ffc-grants-frontend-test.azure.defra.cloud/water')
 const envRoot = (process.env.TEST_ENVIRONMENT_ROOT_URL || 'http://host.docker.internal:3000')
-// const envRoot = 'http://host.docker.internal:3000'
 const chromeArgs = process.env.CHROME_ARGS ? process.env.CHROME_ARGS.split(' ') : []
 const maxInstances = process.env.MAX_INSTANCES ? Number(process.env.MAX_INSTANCES) : 5
-const user = process.env.BROWSERSTACK_USERNAME
-const key = process.env.BROWSERSTACK_ACCESS_KEY
-const parallel = process.env.BROWSERSTACK_PARALLEL_RUNS
 
 exports.config = {
-  hostname: 'hub-cloud.browserstack.com',
-  user,
-  key,
+  hostname: 'selenium',
+  path: '/wd/hub',
   specs: ['./features/**/*.feature'],
   exclude: ['./scratch/**'],
-  parallel,
   maxInstances,
-  capabilities: [
-    {
-      maxInstances,
-      acceptInsecureCerts: true,
-      browserName: 'chrome',
-      'browserstack.local': true,
-      'browserstack.networkLogs': true,
-      'browserstack.acceptSslCerts': true,
-      'goog:chromeOptions': {
-        args: chromeArgs
-      }
+  capabilities: [{
+    maxInstances,
+    acceptInsecureCerts: true,
+    browserName: 'chrome',
+    'goog:chromeOptions': {
+      args: chromeArgs
     }
-    // ,
-    // {
-    //   os: 'Windows',
-    //   osVersion: '10',
-    //   browserName: 'Firefox',
-    //   browserVersion: 'latest',
-    //   'browserstack.local': true,
-    //   acceptInsecureCerts: true,
-    //   acceptSslCerts: true
-    // }
-
-    // {
-    //   // Windows Edge 86
-    //   os: 'Windows',
-    //   osVersion: '10',
-    //   browserName: 'Edge',
-    //   browserVersion: '86',
-    //   'browserstack.local': true,
-    //   acceptInsecureCerts: true,
-    //   acceptSslCerts: true,
-    //   'browserstack.selenium_version': '4.0.0-alpha-6'
-    // }
-
-    // ,
-    // {
-    //   os: 'OS X',
-    //   osVersion: 'Big Sur',
-    //   browserName: 'Safari',
-    //   browserVersion: 'latest',
-    //   'browserstack.local': true,
-    //   acceptInsecureCerts: true,
-    //   acceptSslCerts: true
-    // },
-
-    // {
-    //   // Windows IE 11
-    //   os: 'Windows',
-    //   osVersion: '10',
-    //   browserName: 'IE',
-    //   browserVersion: '11',
-    //   resolution: '1024x768',
-    //   'browserstack.local': true,
-    //   acceptInsecureCerts: true,
-    //   acceptSslCerts: true
-    // },
-
-    // {
-    //   // Windows Edge 86
-    //   os: 'Windows',
-    //   osVersion: '10',
-    //   browserName: 'Edge',
-    //   browserVersion: '86',
-    //   'browserstack.local': true,
-    //   acceptInsecureCerts: true,
-    //   acceptSslCerts: true,
-    //   'browserstack.selenium_version': '4.0.0-alpha-6'
-    // }
-  ],
+  }],
+  //
   // ===================
   // Test Configurations
   // ===================
   // Define all options that are relevant for the WebdriverIO instance here
   logLevel: 'warn',
   bail: 0,
-  baseUrl: envRoot,
+  baseUrl: envRoot + '/selenium:4444',
   waitforTimeout: 10000,
   connectionRetryTimeout: 90000,
-  connectionRetryCount: 1,
-  services: ['browserstack'],
+  connectionRetryCount: 3,
+  services: ['selenium-standalone'],
   framework: 'cucumber',
-  specFileRetries: 2,
+  specFileRetries: 3,
   specFileRetriesDelay: 30,
   reporters: ['spec',
     [HtmlReporter, {
@@ -115,6 +45,7 @@ exports.config = {
       LOG: logger
     }]
   ],
+
   // If you are using Cucumber you need to specify the location of your step definitions.
   cucumberOpts: {
     require: ['./steps/**/*.js'], // <string[]> (file/dir) require files before executing features
@@ -132,44 +63,35 @@ exports.config = {
     timeout: 60000, // <number> timeout for step definitions
     ignoreUndefinedDefinitions: false // <boolean> Enable this config to treat undefined definitions as warnings.
   },
-  // ====
+
+  // =====
   // Hooks
   // =====
   onPrepare: function (config, capabilities) {
+    console.log(`Running tests against: ${envRoot}\n`)
+
     const reportAggregator = new ReportAggregator({
       outputDir: './html-reports/',
       filename: 'acceptance-test-suite-report.html',
       reportTitle: 'Acceptance Tests Report',
       browserName: capabilities.browserName
+      // to use the template override option, can point to your own file in the test project:
+      // templateFilename: path.resolve(__dirname, '../template/wdio-html-reporter-alt-template.hbs')
     })
     reportAggregator.clean()
+
     global.reportAggregator = reportAggregator
-    console.log('Connecting local')
-    return new Promise(function (resolve, reject) {
-      exports.bs_local = new browserstack.Local()
-      const bsLocalArgs = {
-        key,
-        verbose: 'true',
-        onlyAutomate: 'true'
-      }
-      exports.bs_local.start(bsLocalArgs, function (error) {
-        if (error) return reject(error)
-        console.log('Connected. Now testing...')
-        resolve()
-      })
-    })
   },
 
   onComplete: function (exitCode, config, capabilities, results) {
     (async () => {
       await global.reportAggregator.createReport()
     })()
-    exports.bs_local.stop()
-    console.log('Testing complete, binary closed')
   },
 
   beforeSession: function () {
     const chai = require('chai')
+
     global.expect = chai.expect
     global.assert = chai.assert
     global.should = chai.should()
@@ -179,11 +101,14 @@ exports.config = {
     if (result.passed) {
       return
     }
+
     const path = require('path')
     const moment = require('moment')
+
     const screenshotFileName = ctx.uri.split('.feature')[0].split('/').slice(-1)[0]
     const timestamp = moment().format('YYYYMMDD-HHmmss.SSS')
     const filepath = path.join('./html-reports/screenshots/', screenshotFileName + '-' + timestamp + '.png')
+
     browser.saveScreenshot(filepath)
     process.emit('test:screenshot', filepath)
   }
